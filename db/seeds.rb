e473ca3e830fb249072e313805fca184ccaf15b1
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2018-01-21 22:52:37 -0800

    Add db seeds for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+require 'faker'
+
+class Seeds
+  def create_reporters
+    return if Reporter.any?
+
+    11.times { create_user('Reporter') }
+  end
+
+  def create_reviewers
+    return if Reviewer.any?
+
+    3.times { create_user('Reviewer') }
+  end
+
+  def create_workers
+    return if Worker.any?
+
+    5.times { create_user('Worker') }
+  end
+
+  private
+
+    def create_user(employee_type)
+      User.create(name: Faker::Name.name, email: Faker::Internet.email,
+                  employee_type: employee_type)
+    end
+end
+
+seeds = Seeds.new
+seeds.create_reporters
+seeds.create_reviewers
+seeds.create_workers
