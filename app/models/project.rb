cccd9b284ed96f7c217b304ce3a511f559d6d351
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2018-01-24 21:55:31 -0800

    Validate Project#name uniqueness

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,12 +3,12 @@
 # TODO: add friendly_id/slug
 # TODO: add icon options
 # TODO: add image
-# TODO: validate unique name within category
 
 class Project < ApplicationRecord
   belongs_to :category
 
-  validates :name, presence: true, length: { maximum: 250 }
+  validates :name, presence: true, length: { maximum: 250 },
+                   uniqueness: { scope: :category_id, case_sensitive: false }
   validates :category_id, presence: true
   validates :category, presence: true, if: :category_id
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -15,6 +15,10 @@ RSpec.describe Project, type: :model do
   it { is_expected.to belong_to(:category) }
 
   it { is_expected.to validate_presence_of(:name) }
+  it do
+    is_expected.to validate_uniqueness_of(:name)
+      .case_insensitive.scoped_to(:category_id)
+  end
   it { is_expected.to validate_length_of(:name).is_at_most(250) }
   it { is_expected.to validate_presence_of(:category_id) }
 end
