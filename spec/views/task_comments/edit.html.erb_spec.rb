d89a0d195639f73c59bf686318291a42aff833a3
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2018-05-23 16:08:26 -0700

    Start TaskComment views

---
+++ spec/views/task_comments/edit.html.erb_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "task_comments/edit", type: :view do
+  let(:user) { Fabricate(:user_worker) }
+  let(:category) { Fabricate(:category) }
+  let(:project) { Fabricate(:project, category: category) }
+  let(:task) { Fabricate(:task, project: project) }
+  let(:task_comment) { Fabricate(:task_comment, task: task) }
+
+  let(:url) do
+    category_project_task_task_comment_url(category, project, task,
+                                           task_comment)
+  end
+
+  before(:each) do
+    assign(:category, category)
+    assign(:project, project)
+    assign(:task, task)
+    assign(:task_comment, task_comment)
+    assign(:user_options, [["Type 1", [["Name 1", 12], ["Name 2", 14]]]])
+  end
+
+  it "renders the edit task_comment form" do
+    render
+
+    assert_select "form[action=?][method=?]", url, "post" do
+      assert_select "select[name=?]", "task_comment[user_id]"
+
+      assert_select "textarea[name=?]", "task_comment[body]"
+    end
+  end
+end
---
+++ spec/views/task_comments/new.html.erb_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "task_comments/new", type: :view do
+  let(:user) { Fabricate(:user_worker) }
+  let(:category) { Fabricate(:category) }
+  let(:project) { Fabricate(:project, category: category) }
+  let(:task) { Fabricate(:task, project: project) }
+
+  let(:url) do
+    category_project_task_task_comments_url(category, project, task)
+  end
+
+  before(:each) do
+    assign(:category, category)
+    assign(:project, project)
+    assign(:task, task)
+    assign(:task_comment, task.comments.build)
+    assign(:user_options, [["Type 1", [["Name 1", 12], ["Name 2", 14]]]])
+  end
+
+  it "renders new task_comment form" do
+    render
+
+    assert_select "form[action=?][method=?]", url, "post" do
+      assert_select "select[name=?]", "task_comment[user_id]"
+
+      assert_select "textarea[name=?]", "task_comment[body]"
+    end
+  end
+end
