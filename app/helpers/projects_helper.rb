7eacc5611837212a016d2986bede2dafa24aebd5
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2018-05-23 21:38:41 -0700

    Start improving task header

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -17,7 +17,52 @@ module ProjectsHelper
     end
   end
 
+  def breadcrumbs(views)
+    return if views.none?
+
+    content_tag :div, class: 'breadcrumbs' do
+      views.each do |text, url, options|
+        concat breadcrumb(text, url, options)
+      end
+    end
+  end
+
+  def task_header(task, project, category)
+    pages = []
+    pages << [category.name, category] if category
+    pages << project_breadcrumb_item(project) if project
+    pages << issue_breadcrumb_item(category, project, task.issue) if task.issue
+
+    content_tag :header, class: 'issue-header' do
+      concat task_title(task)
+      concat breadcrumbs(pages)
+    end
+  end
+
   private
+
+    def project_breadcrumb_item(project)
+      [project.name, category_project_path(project.category, project)]
+    end
+
+    def issue_breadcrumb_item(category, project, issue)
+      [issue.summary,
+       category_project_issue_path(category, project, issue),
+       id: "task-issue-#{issue.id}"]
+    end
+
+    def breadcrumb(text, url, options = {})
+      content_tag :span, class: 'breadcrumb' do
+        link_to text, url, options
+      end
+    end
+
+    def task_title(task)
+      content_tag :div, class: 'task-title' do
+        concat content_tag :h1, "Task: #{task.summary}", class: 'task-summary'
+        concat task_type_tag task.task_type
+      end
+    end
 
     def project_tag(text, klass)
       content_tag :span, text, class: "tag tag-#{klass}"
