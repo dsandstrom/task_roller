d2e71e161a045fc97b375a9010c4b5e29c868c90
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2020-06-09 03:23:56 -0700

    Add highlight.js
    For syntax highlighting in comments

--- app/javascript/packs/editors.js
+++ app/javascript/packs/editors.js
@@ -7,60 +7,9 @@
 // FIXME: redcarpet requires code block to have emtpy line above
 
 const EasyMDE = require('easymde/dist/easymde.min.js')
+const hljs = require('highlight.js')
 
 const simplyPromise = value => new Promise((resolve, reject) => resolve(value));
-
-const startHightlighting = function(html) {
-  let promises = [];
-  const htmlParts = html.split(/<\/?pre>/g);
-  promises = htmlParts.map(function(value, index) {
-    if (/^<code/.test(value)) {
-      const matches = value.match(/^<code class="lang-(\w+)"/);
-      const language = (matches != null ? matches.length : undefined) ? matches[1] : 'generic';
-      value = value.replace(/^<code[^>]*>|<\/code>$/g, '');
-      value = unescapeHtml(value);
-      return new Promise((resolve, reject) => Rainbow.color(
-        value,
-        language,
-        function(highlighted, l) {
-          let codeClass = "prettyprint rainbow";
-          if (l) { codeClass += ` lang-${l}`; }
-          return resolve(
-            `<pre><code class=\"${codeClass}\">${highlighted}</code></pre>`
-          );
-      }));
-    } else {
-      return simplyPromise(value);
-    }
-  });
-  return Promise.all(promises);
-};
-
-var unescapeHtml = value => value
-  .replace(/&#39;/g, "'")
-  .replace(/&quot;/g, '"')
-  .replace(/&lt;/g, '<')
-  .replace(/&gt;/g, '>')
-  .replace(/&amp;/g, '&');
-
-// TODO: On IE, replace innerHTML and run Rainbow.color()
-// instead of using patchHTML
-// FIXME: not updating both fullscreen preview and split screen preview
-EasyMDE.prototype.renderPreview = function(previewTarget) {
-  const editor = this;
-  // stop rendering preview
-  if (previewTarget === false) { editor.previewElement = null; }
-  if ((typeof previewTarget === 'object') && (previewTarget.nodeType === 1)) {
-    // remember new preview target
-    editor.previewElement = previewTarget;
-  }
-  if (!editor.previewElement) { return; }
-  const html = editor.options.previewRender(editor.value());
-  // startHightlighting(html).then (htmlParts) ->
-  //   # TODO: don't need expose both attributes, just need html
-  //   editor.patchHTML(editor.previewElement, htmlParts.join(''))
-  return true;
-};
 
 class Editor {
   constructor(elem) {
@@ -71,17 +20,23 @@ class Editor {
 
   options() {
     return {
-      element: this.elem,
       autoDownloadFontAwesome: false,
       autofocus: false,
-      // autosave: false
+      autosave: {
+        enabled: false
+      },
       blockStyles: {
         bold: '**',
         italic: '_'
       },
+      element: this.elem,
       placeholder: 'Start typing here...',
       promptURLs: false,
       status: false,
+      renderingConfig: {
+        codeSyntaxHighlighting: true,
+        hljs: hljs
+      },
       toolbar: ['bold', 'italic', 'code', '|',
                 'heading-1', 'strikethrough', 'link', '|',
                 'quote', 'unordered-list', 'ordered-list', '|',
@@ -94,14 +49,12 @@ class Editor {
     return changes = { 'Big Heading': 'Big Heading', 'Heading': 'Heading' };
   }
 }
-    // TODO: change heading
-
-// use turbolinks:load event instead
-// Rainbow.defer = true
 
 document.addEventListener('turbolinks:load', function() {
   // syntax highlight
-  // Rainbow.color() if document.querySelector('.comment pre code')
+  document.querySelectorAll('.comment pre code').forEach((block) => {
+    hljs.highlightBlock(block);
+  });
 
   const taskDescription = document.getElementById('task_description');
   if (taskDescription) {
