6ec3393f71a46c5e160ac2751d5597f5ec6547bc
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2020-06-09 18:32:00 -0700

    Fix Issues show view when user destroyed

--- spec/views/issues/show.html.erb_spec.rb
+++ spec/views/issues/show.html.erb_spec.rb
@@ -74,4 +74,60 @@ RSpec.describe "issues/show", type: :view do
       assert_select ".issue-summary", "Issue: #{@issue.summary}"
     end
   end
+
+  context "when issue's user destroyed" do
+    before do
+      @project = assign(:project, Fabricate(:project, category: @category))
+      @issue = assign(:issue, Fabricate(:issue, project: @project))
+      @comments = assign(:comments, [])
+      @comment = assign(:issue_comment, @issue.comments.build)
+
+      @issue.user.destroy
+      @issue.reload
+    end
+
+    it "renders default user name" do
+      render
+      assert_select ".issue-user", User.destroyed_name
+    end
+  end
+
+  context "when comment's user destroyed" do
+    let(:user) { Fabricate(:user_worker) }
+
+    before do
+      @issue = assign(:issue, Fabricate(:issue))
+      @issue_comment = Fabricate(:issue_comment, issue: @issue, user: user)
+      @comments = assign(:comments, [@issue_comment])
+      @comment = assign(:issue_comment, @issue.comments.build)
+
+      @issue_comment.user.destroy
+      @issue_comment.reload
+    end
+
+    it "renders default user name" do
+      render
+      assert_select "#comment-#{@issue_comment.id} .comment-user",
+                    User.destroyed_name
+    end
+  end
+
+  context "issues's task destroyed" do
+    before do
+      @project = assign(:project, Fabricate(:project, category: @category))
+      @issue = Fabricate(:issue, project: @project)
+      @task = assign(:task, Fabricate(:task, project: @project, issue: @issue))
+      @comment = assign(:issue_comment, @issue.comments.build)
+      @comments = assign(:comments, [])
+
+      @task.destroy
+      @issue.reload
+    end
+
+    it "renders" do
+      expect do
+        render
+      end.not_to raise_error
+    end
+  end
 end
