2703db0e766729ee7ca6eb16ef524fa62647df65
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2020-06-12 22:09:51 -0700

    Validate markdown editor on change
    Had to move editors to form_validation.js so I can pass in the editor
    to be validated instead of the hidden textarea
    
    Need to do some cleanup

--- app/javascript/packs/form_validation.js
+++ app/javascript/packs/form_validation.js
@@ -9,6 +9,51 @@
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
 
+const EasyMDE = require('easymde/dist/easymde.min.js')
+const hljs = require('highlight.js')
+
+class Editor {
+  constructor(elem) {
+    this.elem = elem;
+    this.editor = new EasyMDE(this.options());
+  }
+
+  options() {
+    // just want one heading version (h1, h2, h3 are all the same size)
+    // so button should toggle h1, but look like a generic Heading
+    var headingTool = {
+      name: 'heading-1',
+      action: EasyMDE.toggleHeading1,
+      className: "fa fa-header",
+      title: 'Heading'
+    }
+    return {
+      autoDownloadFontAwesome: false,
+      autofocus: false,
+      autosave: {
+        enabled: false
+      },
+      blockStyles: {
+        bold: '**',
+        italic: '_'
+      },
+      element: this.elem,
+      placeholder: 'Start typing here...',
+      promptURLs: false,
+      status: false,
+      renderingConfig: {
+        codeSyntaxHighlighting: true,
+        hljs: hljs
+      },
+      toolbar: ['bold', 'italic', headingTool, '|',
+                'quote', 'unordered-list', 'ordered-list', '|',
+                'link', 'code', 'strikethrough', '|',
+                'preview', 'side-by-side', 'fullscreen', '|',
+                'guide']
+    };
+  }
+}
+
 const FormValidator = require("validate-js/validate")
 
 var Form = (function() {
@@ -46,9 +91,10 @@ var Form = (function() {
         return elem;
       };
     }
-    constructor(form) {
+    constructor(form, editors) {
       this.afterValidate = this.afterValidate.bind(this);
       this.form = form;
+      this.editors = editors;
       this.button = this.form.querySelector("[type='submit']");
       if (!this.button) { return; }
       this.validator = new FormValidator(this.form.name, this.options(), this.afterValidate);
@@ -120,15 +166,18 @@ var Form = (function() {
 
         inputNode.addEventListener('keyup', (event) => {
           const currentField = fields[event.target.name]
+          if (!currentField) return;
+
           currentField.element = event.target;
           currentField.value = event.target.value;
           currentField.id = event.target.id;
           // clear error for field
+
           form.validator.errors = form.validator.errors.filter(function (obj) {
             return obj.name !== currentField.name;
           });
           form.validator._validateField(currentField);
-          this.afterValidate(form.validator.errors, event);
+          form.afterValidate(form.validator.errors, event);
         })
       });
     }
@@ -137,24 +186,30 @@ var Form = (function() {
       const form = this;
       const fields = form.validator.fields;
 
-      Array.from(this.form.querySelectorAll('textarea')).map((textarea) =>
-        textarea.addEventListener('keyup', (event) => {
-          const fieldNode = event.target.parentNode.parentNode.parentNode;
-          const inputNode = fieldNode.querySelector('textarea');
-          const currentField = fields[inputNode.name];
+      this.editors.forEach((editor) => {
 
-          currentField.element = inputNode;
-          currentField.value = event.target.value;
-          currentField.id = inputNode.id;
+        // https://codemirror.net/doc/manual.html
+        editor.editor.codemirror.on("changes", function(codemirror, changes) {
+          const textarea = codemirror.getTextArea();
+          if (!textarea) return;
+
+          const currentField = fields[textarea.name]
+          if (!currentField) return;
+
+          currentField.element = textarea;
+          currentField.value = codemirror.doc.getValue();
+          currentField.id = textarea.id;
+          console.log(currentField);
 
           // clear error for field
           form.validator.errors = form.validator.errors.filter(function (obj) {
             return obj.name !== currentField.name;
           });
           form.validator._validateField(currentField);
-          this.afterValidate(form.validator.errors, event);
-        })
-      );
+          form.afterValidate(form.validator.errors, event);
+        });
+      });
+
     }
 
     // FIXME: if other fields have errors, fixing the current field doesn't
@@ -182,20 +237,36 @@ var Form = (function() {
   return Form;
 })();
 
-document.addEventListener('turbolinks:load', function() {
-  const formNames = ['issue_type_form', 'task_type_form', 'user_form',
-                     'issue_form', 'category_form', 'project_form',
-                     'task_form', 'task_comment_form', 'issue_comment_form'];
-  return (() => {
-    const result = [];
-    for (let form of Array.from(document.querySelectorAll('form'))) {
-      if (!formNames.includes(form.name)) { continue; }
-      result.push(new Form(form));
-    }
-    return result;
-  })();
-});
-
 function __guard__(value, transform) {
   return (typeof value !== 'undefined' && value !== null) ? transform(value) : undefined;
 }
+
+document.addEventListener('turbolinks:load', function() {
+  // add markdown editor to textareas
+  const editorIds = ['task_description', 'issue_description',
+                     'issue_comment_body', 'task_comment_body'];
+
+  const currentEditors = [];
+  editorIds.forEach((id, i) => {
+    var editorTarget = document.getElementById(id);
+    if (!editorTarget || editorTarget.style.display == 'none') return;
+
+    currentEditors.push(new Editor(editorTarget));
+  });
+
+  // add validation to forms
+  const formNames = ['issue_type_form', 'task_type_form', 'user_form',
+                     'issue_form', 'category_form', 'project_form',
+                     'task_form', 'task_comment_form', 'issue_comment_form'];
+  const currentForms = [];
+
+  for (let form of Array.from(document.querySelectorAll('form'))) {
+    if (!formNames.includes(form.name)) { continue; }
+    currentForms.push(new Form(form, currentEditors));
+  }
+
+  // syntax highlight
+  document.querySelectorAll('.comment pre code').forEach((block) => {
+    hljs.highlightBlock(block);
+  });
+});
