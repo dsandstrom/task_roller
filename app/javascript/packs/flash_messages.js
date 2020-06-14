6b13011f9dd4dd235b4935798d7bdc63f0d3ea8b
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2020-06-14 00:32:14 -0700

    Fix linting errors in flash_messages.js

--- app/javascript/packs/flash_messages.js
+++ app/javascript/packs/flash_messages.js
@@ -1,33 +1,37 @@
-/*
- * decaffeinate suggestions:
- * DS101: Remove unnecessary use of Array.from
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 class FlashMessage {
   constructor(elem) {
     this.close = this.close.bind(this);
     this.elem = elem;
+
     const closeLink = this.elem.querySelector('.close-link');
-    if (!closeLink) { return; }
+    if (!closeLink) return;
+
     closeLink.addEventListener('click', () => this.close());
     this.timeout = null;
     this.autoClose();
   }
 
-  close() { return this.elem.classList.add('hide'); }
+  close() {
+    this.elem.classList.add('hide');
+  }
 
+  // close the flash message after 10 secs, pause while mouse hovered over
   // TODO: test on all browsers
   autoClose() {
     this.timeout = window.setTimeout(this.close, 10000);
-    return this.elem.addEventListener('mouseenter', () => {
+
+    this.elem.addEventListener('mouseenter', () => {
       window.clearTimeout(this.timeout);
-      return this.elem.addEventListener('mouseleave', () => {
-        return this.timeout = window.setTimeout(this.close, 5000);
+
+      this.elem.addEventListener('mouseleave', () => {
+        this.timeout = window.setTimeout(this.close, 5000);
       });
     });
   }
 }
 
-document.addEventListener('turbolinks:load', () => Array.from(document.querySelectorAll('.flash-message')).map((message) =>
-  new FlashMessage(message)));
+document.addEventListener('turbolinks:load', function() {
+  for (var message of document.querySelectorAll('.flash-message')) {
+    new FlashMessage(message);
+  }
+})
