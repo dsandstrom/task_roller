97c40f6a7dcbdeac21e85ec2b4852eeff5abdf9f
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2020-06-14 05:23:15 -0700

    Add todos

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -16,6 +16,11 @@ class IssuesController < ApplicationController
       end
   end
 
+  # TODO: add new task link
+  # TODO: add status to issues/tasks
+  # issue: open, closed, being worked on, duplicate
+  # task: open, done, cancelled, being worked on, duplicate
+  # TODO: connect issue status to tasks
   def show
     @comments = @issue.comments.includes(:user)
     @comment = @issue.comments.build
