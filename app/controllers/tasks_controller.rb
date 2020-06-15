288433192ae38887037042382d0859023b05fa55
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2020-06-15 04:16:42 -0700

    Add issue_id select to Issue/Task form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -100,13 +100,14 @@ class TasksController < ApplicationController
 
     def task_params
       params.require(:task).permit(:summary, :description, :task_type_id,
-                                   :user_id, assignee_ids: [])
+                                   :user_id, :issue_id, assignee_ids: [])
     end
 
     def set_form_options
       set_task_types
       set_user_options
       set_assignee_options
+      set_issue_options
     end
 
     def set_user_options
@@ -120,6 +121,13 @@ class TasksController < ApplicationController
       @assignee_options =
         %w[Reviewer Worker].map do |type|
           [type, User.employees(type).map { |u| [u.name_and_email, u.id] }]
+        end
+    end
+
+    def set_issue_options
+      @issue_options =
+        @project.issues.map do |issue|
+          [issue.short_summary, issue.id]
         end
     end
 end
