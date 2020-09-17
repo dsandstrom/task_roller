e334b44474f269453c311f40cfbe2d130db5ea8b
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2020-09-16 20:18:39 -0700

    Update Issue/IssueConnection relationships
    Add inverse_of

--- app/models/issue_connection.rb
+++ app/models/issue_connection.rb
@@ -2,7 +2,7 @@
 
 class IssueConnection < RollerConnection
   # current issue
-  belongs_to :source, class_name: 'Issue'
+  belongs_to :source, class_name: 'Issue', inverse_of: :source_issue_connections
   # issue current issue duplicates
-  belongs_to :target, class_name: 'Issue'
+  belongs_to :target, class_name: 'Issue', inverse_of: :target_issue_connections
 end
