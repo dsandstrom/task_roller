896dd1b5cf8e687567ca9dccb44c3d5a9b37880a
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2020-09-26 12:25:32 -0700

    Add TaskConnection#target_options

--- app/models/task_connection.rb
+++ app/models/task_connection.rb
@@ -5,4 +5,9 @@ class TaskConnection < RollerConnection
   belongs_to :source, class_name: 'Task'
   # task current task duplicates
   belongs_to :target, class_name: 'Task'
+
+  def target_options
+    @target_options ||=
+      (source.project&.tasks&.where&.not(id: source.id) if source&.id)
+  end
 end
--- spec/models/task_connection_spec.rb
+++ spec/models/task_connection_spec.rb
@@ -3,8 +3,9 @@
 require "rails_helper"
 
 RSpec.describe TaskConnection, type: :model do
-  let(:source) { Fabricate(:task) }
-  let(:target) { Fabricate(:task) }
+  let(:project) { Fabricate(:project) }
+  let(:source) { Fabricate(:task, project: project) }
+  let(:target) { Fabricate(:task, project: project) }
 
   before do
     @task_connection =
@@ -18,4 +19,50 @@ RSpec.describe TaskConnection, type: :model do
 
   it { is_expected.to belong_to(:source) }
   it { is_expected.to belong_to(:target) }
+
+  describe "#target_options" do
+    let(:source) { Fabricate(:task) }
+    let(:task_connection) do
+      Fabricate.build(:task_connection, source: source)
+    end
+
+    context "when source has a project" do
+      context "with other tasks" do
+        it "returns source project's other tasks" do
+          task = Fabricate(:task, project: source.project)
+          expect(task_connection.target_options).to eq([task])
+        end
+      end
+
+      context "with no other tasks" do
+        it "returns source project's other tasks" do
+          expect(task_connection.target_options).to eq([])
+        end
+      end
+    end
+
+    context "when source doesn't have a project" do
+      before { task_connection.source.project = nil }
+
+      it "returns nil" do
+        expect(task_connection.target_options).to be_nil
+      end
+    end
+
+    context "when source nil" do
+      before { task_connection.source = nil }
+
+      it "returns nil" do
+        expect(task_connection.target_options).to be_nil
+      end
+    end
+
+    context "when source id is nil" do
+      before { task_connection.source.id = nil }
+
+      it "returns nil" do
+        expect(task_connection.target_options).to be_nil
+      end
+    end
+  end
 end
