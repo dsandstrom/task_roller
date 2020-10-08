c04b14d963683761f9d868add758938e006210e4
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2020-10-08 15:04:51 -0700

    Add ProjectPolicy

---
+++ spec/policies/project_policy_spec.rb
@@ -0,0 +1,88 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe ProjectPolicy, type: :policy do
+  let(:admin) { Fabricate(:user_admin) }
+  let(:project) { Fabricate(:project) }
+
+  subject { described_class }
+
+  permissions :index?, :show? do
+    %i[admin reviewer worker reporter].each do |employee_type|
+      it "permits #{employee_type}" do
+        user = Fabricate("user_#{employee_type}")
+        expect(subject).to permit(user, project)
+      end
+    end
+
+    it "blocks non-employees" do
+      user = Fabricate(:user)
+      user.employee.destroy
+      expect(subject).not_to permit(user, project)
+    end
+  end
+
+  permissions :create?, :new? do
+    %i[admin reviewer].each do |employee_type|
+      it "permits #{employee_type}" do
+        user = Fabricate("user_#{employee_type}")
+        expect(subject).to permit(user, project)
+      end
+    end
+
+    %i[worker reporter].each do |employee_type|
+      it "blocks #{employee_type}" do
+        user = Fabricate("user_#{employee_type}")
+        expect(subject).not_to permit(user, project)
+      end
+    end
+
+    it "blocks non-employees" do
+      user = Fabricate(:user)
+      user.employee.destroy
+      expect(subject).not_to permit(user, project)
+    end
+  end
+
+  permissions :update?, :edit? do
+    %i[admin reviewer].each do |employee_type|
+      it "permits #{employee_type}" do
+        user = Fabricate("user_#{employee_type}")
+        expect(subject).to permit(user, project)
+      end
+    end
+
+    %i[worker reporter].each do |employee_type|
+      it "blocks #{employee_type}" do
+        user = Fabricate("user_#{employee_type}")
+        expect(subject).not_to permit(user, project)
+      end
+    end
+
+    it "blocks non-employees" do
+      user = Fabricate(:user)
+      user.employee.destroy
+      expect(subject).not_to permit(user, project)
+    end
+  end
+
+  permissions :destroy? do
+    it "permits admins" do
+      expect(subject).to permit(admin, project)
+    end
+
+    %i[reviewer worker reporter].each do |employee_type|
+      it "blocks #{employee_type}" do
+        user = Fabricate("user_#{employee_type}")
+        expect(subject).not_to permit(user, project)
+      end
+    end
+
+    it "blocks non-employees" do
+      user = Fabricate(:user)
+      user.employee.destroy
+      expect(subject).not_to permit(user, project)
+    end
+  end
+end
