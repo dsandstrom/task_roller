502c05de3ce936c98944647baf9e6caae589e511
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2020-10-08 17:58:24 -0700

    Update RepositionIssue(Task)TypesController
    test authorization

--- spec/controllers/reposition_issue_types_controller_spec.rb
+++ spec/controllers/reposition_issue_types_controller_spec.rb
@@ -6,44 +6,59 @@ RSpec.describe RepositionIssueTypesController, type: :controller do
   let(:valid_attributes) { "up" }
   let(:invalid_attributes) { "" }
 
-  before { login(Fabricate(:user_admin)) }
-
   describe "PUT #update" do
     before { Fabricate(:issue_type) }
 
-    context "with valid params" do
-      it "updates the requested issue_type's position" do
-        issue_type = Fabricate(:issue_type)
-        expect do
+    context "for an admin" do
+      before { login(Fabricate(:user_admin)) }
+
+      context "with valid params" do
+        it "updates the requested issue_type's position" do
+          issue_type = Fabricate(:issue_type)
+          expect do
+            put :update, params: { id: issue_type.to_param,
+                                   sort: valid_attributes }
+            issue_type.reload
+          end.to change(issue_type, :position).from(2).to(1)
+        end
+
+        it "redirects to the issue_type list" do
+          issue_type = Fabricate(:issue_type)
           put :update, params: { id: issue_type.to_param,
                                  sort: valid_attributes }
-          issue_type.reload
-        end.to change(issue_type, :position).from(2).to(1)
+          expect(response).to redirect_to(roller_types_url)
+        end
       end
 
-      it "redirects to the issue_type list" do
-        issue_type = Fabricate(:issue_type)
-        put :update, params: { id: issue_type.to_param,
-                               sort: valid_attributes }
-        expect(response).to redirect_to(roller_types_url)
+      context "with invalid params" do
+        it "doesn't update the requested issue_type" do
+          issue_type = Fabricate(:issue_type)
+          expect do
+            put :update, params: { id: issue_type.to_param,
+                                   sort: invalid_attributes }
+            issue_type.reload
+          end.not_to change(issue_type, :position)
+        end
+
+        it "redirects to the issue_type list" do
+          issue_type = Fabricate(:issue_type)
+          put :update, params: { id: issue_type.to_param,
+                                 sort: invalid_attributes }
+          expect(response).to redirect_to(roller_types_url)
+        end
       end
     end
 
-    context "with invalid params" do
-      it "doesn't update the requested issue_type" do
-        issue_type = Fabricate(:issue_type)
-        expect do
-          put :update, params: { id: issue_type.to_param,
-                                 sort: invalid_attributes }
-          issue_type.reload
-        end.not_to change(issue_type, :position)
-      end
+    %w[reviewer worker reporter].each do |employee_type|
+      context "for a #{employee_type}" do
+        before { login(Fabricate("user_#{employee_type}")) }
 
-      it "redirects to the issue_type list" do
-        issue_type = Fabricate(:issue_type)
-        put :update, params: { id: issue_type.to_param,
-                               sort: invalid_attributes }
-        expect(response).to redirect_to(roller_types_url)
+        it "redirects to unauthorized" do
+          issue_type = Fabricate(:issue_type)
+          put :update, params: { id: issue_type.to_param,
+                                 sort: valid_attributes }
+          expect_to_be_unauthorized(response)
+        end
       end
     end
   end
--- spec/controllers/reposition_task_types_controller_spec.rb
+++ spec/controllers/reposition_task_types_controller_spec.rb
@@ -6,44 +6,59 @@ RSpec.describe RepositionTaskTypesController, type: :controller do
   let(:valid_attributes) { "up" }
   let(:invalid_attributes) { "" }
 
-  before { login(Fabricate(:user_admin)) }
-
   describe "PUT #update" do
     before { Fabricate(:task_type) }
 
-    context "with valid params" do
-      it "updates the requested task_type's position" do
-        task_type = Fabricate(:task_type)
-        expect do
+    context "for an admin" do
+      before { login(Fabricate(:user_admin)) }
+
+      context "with valid params" do
+        it "updates the requested task_type's position" do
+          task_type = Fabricate(:task_type)
+          expect do
+            put :update, params: { id: task_type.to_param,
+                                   sort: valid_attributes }
+            task_type.reload
+          end.to change(task_type, :position).from(2).to(1)
+        end
+
+        it "redirects to the task_type list" do
+          task_type = Fabricate(:task_type)
           put :update, params: { id: task_type.to_param,
                                  sort: valid_attributes }
-          task_type.reload
-        end.to change(task_type, :position).from(2).to(1)
+          expect(response).to redirect_to(roller_types_url)
+        end
       end
 
-      it "redirects to the task_type list" do
-        task_type = Fabricate(:task_type)
-        put :update, params: { id: task_type.to_param,
-                               sort: valid_attributes }
-        expect(response).to redirect_to(roller_types_url)
+      context "with invalid params" do
+        it "doesn't update the requested task_type" do
+          task_type = Fabricate(:task_type)
+          expect do
+            put :update, params: { id: task_type.to_param,
+                                   sort: invalid_attributes }
+            task_type.reload
+          end.not_to change(task_type, :position)
+        end
+
+        it "redirects to the task_type list" do
+          task_type = Fabricate(:task_type)
+          put :update, params: { id: task_type.to_param,
+                                 sort: invalid_attributes }
+          expect(response).to redirect_to(roller_types_url)
+        end
       end
     end
 
-    context "with invalid params" do
-      it "doesn't update the requested task_type" do
-        task_type = Fabricate(:task_type)
-        expect do
-          put :update, params: { id: task_type.to_param,
-                                 sort: invalid_attributes }
-          task_type.reload
-        end.not_to change(task_type, :position)
-      end
+    %w[reviewer worker reporter].each do |employee_type|
+      context "for a #{employee_type}" do
+        before { login(Fabricate("user_#{employee_type}")) }
 
-      it "redirects to the task_type list" do
-        task_type = Fabricate(:task_type)
-        put :update, params: { id: task_type.to_param,
-                               sort: invalid_attributes }
-        expect(response).to redirect_to(roller_types_url)
+        it "redirects to unauthorized" do
+          task_type = Fabricate(:task_type)
+          put :update, params: { id: task_type.to_param,
+                                 sort: valid_attributes }
+          expect_to_be_unauthorized(response)
+        end
       end
     end
   end
