67039b53f36218b05170d154d1daf779205017a4
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2020-10-10 17:16:40 -0700

    Add authorization to Issue views

--- spec/views/issues/index.html.erb_spec.rb
+++ spec/views/issues/index.html.erb_spec.rb
@@ -3,75 +3,131 @@
 require "rails_helper"
 
 RSpec.describe "issues/index", type: :view do
-  context "when no category and project" do
-    let(:first_issue) { Fabricate(:issue) }
-    let(:second_issue) { Fabricate(:issue) }
+  context "for an admin" do
+    let(:admin) { Fabricate(:user_admin) }
 
-    before(:each) { assign(:issues, [first_issue, second_issue]) }
+    before { enable_pundit(view, admin) }
 
-    it "renders a list of issues" do
-      render
-      assert_select "#issue-#{first_issue.id}"
-      assert_select "#issue-#{second_issue.id}"
+    context "when no category and project" do
+      let(:first_issue) { Fabricate(:issue) }
+      let(:second_issue) { Fabricate(:issue) }
+
+      before(:each) { assign(:issues, [first_issue, second_issue]) }
+
+      it "renders a list of issues" do
+        render
+        assert_select "#issue-#{first_issue.id}"
+        assert_select "#issue-#{second_issue.id}"
+      end
+    end
+
+    context "when only category" do
+      let(:category) { Fabricate(:category) }
+      let(:first_issue) do
+        Fabricate(:issue, project: Fabricate(:project, category: category))
+      end
+      let(:second_issue) do
+        Fabricate(:issue, project: Fabricate(:project, category: category))
+      end
+
+      before(:each) do
+        assign(:category, category)
+        assign(:issues, [first_issue, second_issue])
+      end
+
+      it "renders a list of issues" do
+        render
+        assert_select "#issue-#{first_issue.id}"
+        assert_select "#issue-#{second_issue.id}"
+      end
+    end
+
+    context "when project" do
+      let(:category) { Fabricate(:category) }
+      let(:project) { Fabricate(:project, category: category) }
+      let(:first_issue) { Fabricate(:issue, project: project) }
+      let(:second_issue) { Fabricate(:issue, project: project) }
+
+      before(:each) do
+        assign(:category, category)
+        assign(:project, project)
+        assign(:issues, [first_issue, second_issue])
+      end
+
+      it "renders a list of issues" do
+        render
+
+        assert_select "#issue-#{first_issue.id}"
+        url = edit_category_project_issue_path(category, project, first_issue)
+        expect(rendered).to have_link(nil, href: url)
+        assert_select "#issue-#{second_issue.id}"
+        url = edit_category_project_issue_path(category, project, second_issue)
+        expect(rendered).to have_link(nil, href: url)
+      end
+
+      it "renders new issue link" do
+        render
+
+        url = new_category_project_issue_path(category, project)
+        expect(rendered).to have_link(nil, href: url)
+      end
+    end
+
+    context "when an issue user was destroyed" do
+      let(:first_issue) { Fabricate(:issue) }
+      let(:second_issue) { Fabricate(:issue) }
+
+      before(:each) do
+        second_issue.user.destroy
+        second_issue.reload
+        assign(:issues, [first_issue, second_issue])
+      end
+
+      it "renders a list of issues" do
+        render
+        assert_select "#issue-#{first_issue.id} .issue-user",
+                      first_issue.user.name
+        assert_select "#issue-#{second_issue.id} .issue-user",
+                      User.destroyed_name
+      end
     end
   end
 
-  context "when only category" do
-    let(:category) { Fabricate(:category) }
-    let(:first_issue) do
-      Fabricate(:issue, project: Fabricate(:project, category: category))
-    end
-    let(:second_issue) do
-      Fabricate(:issue, project: Fabricate(:project, category: category))
-    end
+  %w[reviewer worker reporter].each do |employee_type|
+    context "for a #{employee_type}" do
+      let(:current_user) { Fabricate("user_#{employee_type}") }
+      let(:category) { Fabricate(:category) }
+      let(:project) { Fabricate(:project, category: category) }
+      let(:first_issue) do
+        Fabricate(:issue, project: project, user: current_user)
+      end
+      let(:second_issue) { Fabricate(:issue, project: project) }
 
-    before(:each) do
-      assign(:category, category)
-      assign(:issues, [first_issue, second_issue])
-    end
+      before { enable_pundit(view, current_user) }
 
-    it "renders a list of issues" do
-      render
-      assert_select "#issue-#{first_issue.id}"
-      assert_select "#issue-#{second_issue.id}"
-    end
-  end
+      before(:each) do
+        assign(:category, category)
+        assign(:project, project)
+        assign(:issues, [first_issue, second_issue])
+      end
 
-  context "when project" do
-    let(:category) { Fabricate(:category) }
-    let(:project) { Fabricate(:project) }
-    let(:first_issue) { Fabricate(:issue, project: project) }
-    let(:second_issue) { Fabricate(:issue, project: project) }
+      it "renders a list of issues" do
+        render
 
-    before(:each) do
-      assign(:category, category)
-      assign(:project, project)
-      assign(:issues, [first_issue, second_issue])
-    end
+        assert_select "#issue-#{first_issue.id}"
+        url = edit_category_project_issue_path(category, project, first_issue)
+        expect(rendered).to have_link(nil, href: url)
+        assert_select "#issue-#{second_issue.id}"
+        url = edit_category_project_issue_path(category, project, second_issue)
+        expect(rendered).not_to have_link(nil, href: url)
+      end
 
-    it "renders a list of issues" do
-      render
-      assert_select "#issue-#{first_issue.id}"
-      assert_select "#issue-#{second_issue.id}"
-    end
-  end
+      it "renders new issue link" do
+        render
 
-  context "when an issue user was destroyed" do
-    let(:first_issue) { Fabricate(:issue) }
-    let(:second_issue) { Fabricate(:issue) }
-
-    before(:each) do
-      second_issue.user.destroy
-      second_issue.reload
-      assign(:issues, [first_issue, second_issue])
-    end
-
-    it "renders a list of issues" do
-      render
-      assert_select "#issue-#{first_issue.id} .issue-user",
-                    first_issue.user.name
-      assert_select "#issue-#{second_issue.id} .issue-user",
-                    User.destroyed_name
+        url = new_category_project_issue_path(category, project)
+        expect(rendered).to have_link(nil, href: url)
+      end
     end
   end
 end
--- spec/views/issues/show.html.erb_spec.rb
+++ spec/views/issues/show.html.erb_spec.rb
@@ -5,129 +5,230 @@ require "rails_helper"
 RSpec.describe "issues/show", type: :view do
   before(:each) { @category = assign(:category, Fabricate(:category)) }
 
-  context "when project" do
-    before do
-      @project = assign(:project, Fabricate(:project, category: @category))
-      @issue = assign(:issue, Fabricate(:issue, project: @project))
-      @comment = assign(:issue_comment, @issue.comments.build)
-      @comments = assign(:comments, [])
-    end
+  context "for an admin" do
+    let(:admin) { Fabricate(:user_admin) }
 
-    let(:url) do
-      category_project_issue_issue_comments_url(@category, @project, @issue)
-    end
+    before { enable_pundit(view, admin) }
 
-    it "renders issue's heading" do
-      render
-      assert_select ".issue-heading", @issue.heading
-    end
+    context "when project" do
+      before do
+        @project = assign(:project, Fabricate(:project, category: @category))
+        @issue = assign(:issue, Fabricate(:issue, project: @project))
+        @comment = assign(:issue_comment, @issue.comments.build)
+        @comments = assign(:comments, [])
+      end
 
-    it "renders issue's description" do
-      render
-      assert_select ".issue-description main", @issue.description
-    end
+      let(:url) do
+        category_project_issue_issue_comments_url(@category, @project, @issue)
+      end
 
-    it "renders new issue_comment form" do
-      render
+      it "renders issue's heading" do
+        render
+        assert_select ".issue-heading", @issue.heading
+      end
 
-      assert_select "form[action=?][method=?]", url, "post" do
-        assert_select "textarea[name=?]", "issue_comment[body]"
+      it "renders issue's description" do
+        render
+        assert_select ".issue-description main", @issue.description
+      end
+
+      it "renders new issue_comment form" do
+        render
+
+        assert_select "form[action=?][method=?]", url, "post" do
+          assert_select "textarea[name=?]", "issue_comment[body]"
+        end
+      end
+
+      it "renders edit link" do
+        render
+
+        url = edit_category_project_issue_path(@category, @project, @issue)
+        expect(rendered).to have_link(nil, href: url)
+      end
+
+      context "with tasks" do
+        it "renders a list of tasks" do
+          task = Fabricate(:task, issue: @issue)
+
+          render
+          assert_select "#task-#{task.id}"
+        end
       end
     end
 
-    context "with tasks" do
-      it "renders a list of tasks" do
-        task = Fabricate(:task, issue: @issue)
+    context "when no project" do
+      before do
+        project = Fabricate(:project, category: @category)
+        @issue = assign(:issue, Fabricate(:issue, project: project))
+        @comment = assign(:issue_comment, @issue.comments.build)
+        @comments = assign(:comments, [])
+      end
 
+      it "renders heading" do
         render
-        assert_select "#task-#{task.id}"
+        assert_select ".issue-heading", @issue.heading
+      end
+    end
+
+    context "when no issue_type" do
+      before do
+        @project = assign(:project, Fabricate(:project, category: @category))
+        @issue = assign(:issue, Fabricate(:issue, project: @project))
+        @comment = assign(:issue_comment, @issue.comments.build)
+        @comments = assign(:comments, [])
+        @issue.issue_type.destroy
+        @issue.reload
+      end
+
+      it "renders heading" do
+        render
+        assert_select ".issue-heading", @issue.heading
+      end
+    end
+
+    context "when issue's user destroyed" do
+      before do
+        @project = assign(:project, Fabricate(:project, category: @category))
+        @issue = assign(:issue, Fabricate(:issue, project: @project))
+        @comments = assign(:comments, [])
+        @comment = assign(:issue_comment, @issue.comments.build)
+
+        @issue.user.destroy
+        @issue.reload
+      end
+
+      it "renders default user name" do
+        render
+        assert_select ".issue-user", User.destroyed_name
+      end
+    end
+
+    context "when comment's user destroyed" do
+      let(:user) { Fabricate(:user_worker) }
+
+      before do
+        @issue = assign(:issue, Fabricate(:issue))
+        @issue_comment = Fabricate(:issue_comment, issue: @issue, user: user)
+        @comments = assign(:comments, [@issue_comment])
+        @comment = assign(:issue_comment, @issue.comments.build)
+
+        @issue_comment.user.destroy
+        @issue_comment.reload
+      end
+
+      it "renders default user name" do
+        render
+        assert_select "#comment-#{@issue_comment.id} .comment-user",
+                      User.destroyed_name
+      end
+    end
+
+    context "issues's task destroyed" do
+      before do
+        @project = assign(:project, Fabricate(:project, category: @category))
+        @issue = Fabricate(:issue, project: @project)
+        @task =
+          assign(:task, Fabricate(:task, project: @project, issue: @issue))
+        @comment = assign(:issue_comment, @issue.comments.build)
+        @comments = assign(:comments, [])
+
+        @task.destroy
+        @issue.reload
+      end
+
+      it "renders" do
+        expect do
+          render
+        end.not_to raise_error
       end
     end
   end
 
-  context "when no project" do
-    before do
-      project = Fabricate(:project, category: @category)
-      @issue = assign(:issue, Fabricate(:issue, project: project))
-      @comment = assign(:issue_comment, @issue.comments.build)
-      @comments = assign(:comments, [])
-    end
+  %w[reviewer worker reporter].each do |employee_type|
+    context "for a #{employee_type}" do
+      let(:current_user) { Fabricate("user_#{employee_type}") }
 
-    it "renders heading" do
-      render
-      assert_select ".issue-heading", @issue.heading
-    end
-  end
+      before { enable_pundit(view, current_user) }
 
-  context "when no issue_type" do
-    before do
-      @project = assign(:project, Fabricate(:project, category: @category))
-      @issue = assign(:issue, Fabricate(:issue, project: @project))
-      @comment = assign(:issue_comment, @issue.comments.build)
-      @comments = assign(:comments, [])
-      @issue.issue_type.destroy
-      @issue.reload
-    end
+      before do
+        @project = assign(:project, Fabricate(:project, category: @category))
+      end
 
-    it "renders heading" do
-      render
-      assert_select ".issue-heading", @issue.heading
-    end
-  end
+      context "when their issue" do
+        let(:url) do
+          category_project_issue_issue_comments_url(@category, @project, @issue)
+        end
+        let(:issue) { Fabricate(:issue, project: @project, user: current_user) }
 
-  context "when issue's user destroyed" do
-    before do
-      @project = assign(:project, Fabricate(:project, category: @category))
-      @issue = assign(:issue, Fabricate(:issue, project: @project))
-      @comments = assign(:comments, [])
-      @comment = assign(:issue_comment, @issue.comments.build)
+        before do
+          @issue = assign(:issue, issue)
+          @comment = assign(:issue_comment, @issue.comments.build)
+          @comments = assign(:comments, [])
+        end
 
-      @issue.user.destroy
-      @issue.reload
-    end
+        it "renders issue's heading" do
+          render
+          assert_select ".issue-heading", @issue.heading
+        end
 
-    it "renders default user name" do
-      render
-      assert_select ".issue-user", User.destroyed_name
-    end
-  end
+        it "renders issue's description" do
+          render
+          assert_select ".issue-description main", @issue.description
+        end
 
-  context "when comment's user destroyed" do
-    let(:user) { Fabricate(:user_worker) }
+        it "renders new issue_comment form" do
+          render
 
-    before do
-      @issue = assign(:issue, Fabricate(:issue))
-      @issue_comment = Fabricate(:issue_comment, issue: @issue, user: user)
-      @comments = assign(:comments, [@issue_comment])
-      @comment = assign(:issue_comment, @issue.comments.build)
+          assert_select "form[action=?][method=?]", url, "post" do
+            assert_select "textarea[name=?]", "issue_comment[body]"
+          end
+        end
 
-      @issue_comment.user.destroy
-      @issue_comment.reload
-    end
+        it "renders edit link" do
+          render
 
-    it "renders default user name" do
-      render
-      assert_select "#comment-#{@issue_comment.id} .comment-user",
-                    User.destroyed_name
-    end
-  end
+          url = edit_category_project_issue_path(@category, @project, @issue)
+          expect(rendered).to have_link(nil, href: url)
+        end
+      end
 
-  context "issues's task destroyed" do
-    before do
-      @project = assign(:project, Fabricate(:project, category: @category))
-      @issue = Fabricate(:issue, project: @project)
-      @task = assign(:task, Fabricate(:task, project: @project, issue: @issue))
-      @comment = assign(:issue_comment, @issue.comments.build)
-      @comments = assign(:comments, [])
+      context "when someone else's issue" do
+        let(:url) do
+          category_project_issue_issue_comments_url(@category, @project, @issue)
+        end
 
-      @task.destroy
-      @issue.reload
-    end
+        before do
+          @issue = assign(:issue, Fabricate(:issue, project: @project))
+          @comment = assign(:issue_comment, @issue.comments.build)
+          @comments = assign(:comments, [])
+        end
 
-    it "renders" do
-      expect do
-        render
-      end.not_to raise_error
+        it "renders issue's heading" do
+          render
+          assert_select ".issue-heading", @issue.heading
+        end
+
+        it "renders issue's description" do
+          render
+          assert_select ".issue-description main", @issue.description
+        end
+
+        it "renders new issue_comment form" do
+          render
+
+          assert_select "form[action=?][method=?]", url, "post" do
+            assert_select "textarea[name=?]", "issue_comment[body]"
+          end
+        end
+
+        it "doesn't render the edit link" do
+          render
+
+          url = edit_category_project_issue_path(@category, @project, @issue)
+          expect(rendered).not_to have_link(nil, href: url)
+        end
+      end
     end
   end
 end
