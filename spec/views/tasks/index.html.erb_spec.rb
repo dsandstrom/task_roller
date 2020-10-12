0c239ffecbdf37831794cf8ece4b4f625eb548e1
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2020-10-11 21:25:43 -0700

    Add authorization to task views

--- spec/views/tasks/index.html.erb_spec.rb
+++ spec/views/tasks/index.html.erb_spec.rb
@@ -3,75 +3,130 @@
 require "rails_helper"
 
 RSpec.describe "tasks/index", type: :view do
-  context "when no category and project" do
-    let(:first_task) { Fabricate(:task) }
-    let(:second_task) { Fabricate(:task) }
+  context "for an admin" do
+    let(:admin) { Fabricate(:user_admin) }
 
-    before(:each) { assign(:tasks, [first_task, second_task]) }
+    before { enable_pundit(view, admin) }
 
-    it "renders a list of tasks" do
-      render
-      assert_select "#task-#{first_task.id}"
-      assert_select "#task-#{second_task.id}"
+    context "when no category and project" do
+      let(:first_task) { Fabricate(:task) }
+      let(:second_task) { Fabricate(:task) }
+
+      before(:each) { assign(:tasks, [first_task, second_task]) }
+
+      it "renders a list of tasks" do
+        render
+        assert_select "#task-#{first_task.id}"
+        assert_select "#task-#{second_task.id}"
+      end
+    end
+
+    context "when only category" do
+      let(:category) { Fabricate(:category) }
+      let(:first_task) do
+        Fabricate(:task, project: Fabricate(:project, category: category))
+      end
+      let(:second_task) do
+        Fabricate(:task, project: Fabricate(:project, category: category))
+      end
+
+      before(:each) do
+        assign(:category, category)
+        assign(:tasks, [first_task, second_task])
+      end
+
+      it "renders a list of tasks" do
+        render
+        assert_select "#task-#{first_task.id}"
+        assert_select "#task-#{second_task.id}"
+      end
+    end
+
+    context "when project" do
+      let(:category) { Fabricate(:category) }
+      let(:project) { Fabricate(:project, category: category) }
+      let(:first_task) { Fabricate(:task, project: project) }
+      let(:issue) { Fabricate(:issue, project: project) }
+      let(:second_task) { Fabricate(:task, project: project, issue: issue) }
+
+      before(:each) do
+        assign(:category, category)
+        assign(:project, project)
+        assign(:tasks, [first_task, second_task])
+      end
+
+      it "renders a list of tasks" do
+        render
+
+        assert_select "#task-#{first_task.id}"
+        first_url =
+          edit_category_project_task_path(category, project, first_task)
+        expect(rendered).to have_link(nil, href: first_url)
+        assert_select "#task-#{second_task.id}"
+        second_url =
+          edit_category_project_task_path(category, project, second_task)
+        expect(rendered).to have_link(nil, href: second_url)
+        show_url = category_project_issue_path(category, project, issue)
+        expect(rendered).to have_link(nil, href: show_url)
+      end
+    end
+
+    context "when a task user was destroyed" do
+      let(:first_task) { Fabricate(:task) }
+      let(:second_task) { Fabricate(:task) }
+
+      before(:each) do
+        second_task.user.destroy
+        second_task.reload
+        assign(:tasks, [first_task, second_task])
+      end
+
+      it "renders a list of tasks" do
+        render
+        assert_select "#task-#{first_task.id} .task-user",
+                      "Reviewer: #{first_task.user.name}"
+        assert_select "#task-#{second_task.id} .task-user",
+                      "Reviewer: #{User.destroyed_name}"
+      end
     end
   end
 
-  context "when only category" do
-    let(:category) { Fabricate(:category) }
-    let(:first_task) do
-      Fabricate(:task, project: Fabricate(:project, category: category))
-    end
-    let(:second_task) do
-      Fabricate(:task, project: Fabricate(:project, category: category))
-    end
+  %w[reviewer worker reporter].each do |employee_type|
+    context "for a #{employee_type}" do
+      let(:current_user) { Fabricate("user_#{employee_type}") }
 
-    before(:each) do
-      assign(:category, category)
-      assign(:tasks, [first_task, second_task])
-    end
+      before { enable_pundit(view, current_user) }
 
-    it "renders a list of tasks" do
-      render
-      assert_select "#task-#{first_task.id}"
-      assert_select "#task-#{second_task.id}"
-    end
-  end
+      context "when project" do
+        let(:category) { Fabricate(:category) }
+        let(:project) { Fabricate(:project, category: category) }
+        let(:first_task) do
+          Fabricate(:task, project: project, user: current_user)
+        end
+        let(:issue) { Fabricate(:issue, project: project) }
+        let(:second_task) { Fabricate(:task, project: project, issue: issue) }
 
-  context "when project" do
-    let(:category) { Fabricate(:category) }
-    let(:project) { Fabricate(:project) }
-    let(:first_task) { Fabricate(:task, project: project) }
-    let(:second_task) { Fabricate(:task, project: project) }
+        before(:each) do
+          assign(:category, category)
+          assign(:project, project)
+          assign(:tasks, [first_task, second_task])
+        end
 
-    before(:each) do
-      assign(:category, category)
-      assign(:project, project)
-      assign(:tasks, [first_task, second_task])
-    end
+        it "renders a list of tasks" do
+          render
 
-    it "renders a list of tasks" do
-      render
-      assert_select "#task-#{first_task.id}"
-      assert_select "#task-#{second_task.id}"
-    end
-  end
-
-  context "when a task user was destroyed" do
-    let(:first_task) { Fabricate(:task) }
-    let(:second_task) { Fabricate(:task) }
-
-    before(:each) do
-      second_task.user.destroy
-      second_task.reload
-      assign(:tasks, [first_task, second_task])
-    end
-
-    it "renders a list of tasks" do
-      render
-      assert_select "#task-#{first_task.id} .task-user",
-                    "Reviewer: #{first_task.user.name}"
-      assert_select "#task-#{second_task.id} .task-user",
-                    "Reviewer: #{User.destroyed_name}"
+          assert_select "#task-#{first_task.id}"
+          first_url =
+            edit_category_project_task_path(category, project, first_task)
+          expect(rendered).to have_link(nil, href: first_url)
+          assert_select "#task-#{second_task.id}"
+          second_url =
+            edit_category_project_task_path(category, project, second_task)
+          expect(rendered).not_to have_link(nil, href: second_url)
+          show_url = category_project_issue_path(category, project, issue)
+          expect(rendered).to have_link(nil, href: show_url)
+        end
+      end
     end
   end
 end
--- spec/views/tasks/show.html.erb_spec.rb
+++ spec/views/tasks/show.html.erb_spec.rb
@@ -3,174 +3,148 @@
 require "rails_helper"
 
 RSpec.describe "tasks/show", type: :view do
+  let(:admin) { Fabricate(:user_admin) }
+
   before(:each) { @category = assign(:category, Fabricate(:category)) }
 
-  context "when project" do
-    before do
-      @project = assign(:project, Fabricate(:project, category: @category))
-      @task = assign(:task, Fabricate(:task, project: @project))
-    end
+  context "for an admin" do
+    let(:admin) { Fabricate(:user_admin) }
 
-    let(:url) do
-      category_project_task_task_comments_url(@category, @project, @task)
-    end
+    before { enable_pundit(view, admin) }
 
-    it "renders task's heading" do
-      render
-      assert_select ".task-heading", @task.heading
-    end
-
-    it "renders task's description" do
-      render
-      assert_select ".task-description main", @task.description
-    end
-
-    it "renders task's user" do
-      render
-      assert_select ".task-user", @task.user.name_or_email
-    end
-
-    it "renders new task_comment form" do
-      render
-
-      assert_select "form[action=?][method=?]", url, "post" do
-        assert_select "textarea[name=?]", "task_comment[body]"
-      end
-    end
-
-    context "task belongs to an issue" do
-      let(:issue) { Fabricate(:issue, project: @project) }
-
-      before do
-        @task = assign(:task, Fabricate(:task, project: @project, issue: issue))
-      end
-
-      it "renders issue" do
-        render
-        assert_select "#task-issue-#{issue.id}"
-      end
-    end
-
-    context "task assigned to a user" do
-      let(:user) { Fabricate(:user_worker) }
-
-      before do
-        @task = assign(:task, Fabricate(:task, assignee_ids: [user.id]))
-      end
-
-      it "renders assignee" do
-        render
-        assert_select "#assignee-#{user.id}"
-      end
-    end
-
-    context "when task has comments" do
-      let(:user) { Fabricate(:user_worker) }
-
-      before do
-        @task = assign(:task, Fabricate(:task, assignee_ids: [user.id]))
-        @task_comment = Fabricate(:task_comment, task: @task)
-      end
-
-      it "renders them" do
-        render
-        assert_select "#comment-#{@task_comment.id}"
-      end
-    end
-  end
-
-  context "when no task_type" do
-    before do
-      @project = assign(:project, Fabricate(:project, category: @category))
-      @task = assign(:task, Fabricate(:task, project: @project))
-
-      @task.task_type.destroy
-      @task.reload
-    end
-
-    it "renders heading" do
-      render
-      assert_select ".task-heading", @task.heading
-    end
-  end
-
-  context "when task's user destroyed" do
-    before do
-      @project = assign(:project, Fabricate(:project, category: @category))
-      @task = assign(:task, Fabricate(:task, project: @project))
-
-      @task.user.destroy
-      @task.reload
-    end
-
-    it "renders default user name" do
-      render
-      assert_select ".task-user", User.destroyed_name
-    end
-  end
-
-  context "when comment's user destroyed" do
-    let(:user) { Fabricate(:user_worker) }
-
-    before do
-      @project = assign(:project, Fabricate(:project, category: @category))
-      @task = assign(:task, Fabricate(:task, project: @project))
-      @task_comment = Fabricate(:task_comment, task: @task, user: user)
-
-      @task_comment.user.destroy
-      @task_comment.reload
-    end
-
-    it "renders default user name" do
-      render
-      assert_select "#comment-#{@task_comment.id} .comment-user",
-                    User.destroyed_name
-    end
-  end
-
-  context "when task's assignee destroyed" do
-    let(:user) { Fabricate(:user_worker) }
-
-    before do
-      @project = assign(:project, Fabricate(:project, category: @category))
-      task = Fabricate(:task, project: @project, assignee_ids: [user.id])
-      @task = assign(:task, task)
-
-      user.destroy
-      @task.reload
-    end
-
-    it "renders" do
-      expect do
-        render
-      end.not_to raise_error
-    end
-  end
-
-  context "task's issue destroyed" do
-    before do
-      @project = assign(:project, Fabricate(:project, category: @category))
-      @issue = Fabricate(:issue, project: @project)
-      @task = assign(:task, Fabricate(:task, project: @project, issue: @issue))
-
-      @issue.destroy
-      @task.reload
-    end
-
-    it "renders" do
-      expect do
-        render
-      end.not_to raise_error
-    end
-  end
-
-  # TODO: add specs for assignees, progressions, reviews, open, closed
-  context "task closed" do
-    context "with no reviews" do
+    context "when project" do
       before do
         @project = assign(:project, Fabricate(:project, category: @category))
-        @task = assign(:task, Fabricate(:closed_task, project: @project))
+        @task = assign(:task, Fabricate(:task, project: @project))
+      end
 
+      let(:url) do
+        category_project_task_task_comments_url(@category, @project, @task)
+      end
+
+      it "renders task's heading" do
+        render
+        assert_select ".task-heading", @task.heading
+      end
+
+      it "renders task's description" do
+        render
+        assert_select ".task-description main", @task.description
+      end
+
+      it "renders task's user" do
+        render
+        assert_select ".task-user", @task.user.name_or_email
+      end
+
+      it "renders new task_comment form" do
+        render
+
+        assert_select "form[action=?][method=?]", url, "post" do
+          assert_select "textarea[name=?]", "task_comment[body]"
+        end
+      end
+
+      context "task belongs to an issue" do
+        let(:issue) { Fabricate(:issue, project: @project) }
+
+        before do
+          @task =
+            assign(:task, Fabricate(:task, project: @project, issue: issue))
+        end
+
+        it "renders issue" do
+          render
+          assert_select "#task-issue-#{issue.id}"
+        end
+      end
+
+      context "task assigned to a user" do
+        let(:user) { Fabricate(:user_worker) }
+
+        before do
+          @task = assign(:task, Fabricate(:task, assignee_ids: [user.id]))
+        end
+
+        it "renders assignee" do
+          render
+          assert_select "#assignee-#{user.id}"
+        end
+      end
+
+      context "when task has comments" do
+        let(:user) { Fabricate(:user_worker) }
+
+        before do
+          @task = assign(:task, Fabricate(:task, assignee_ids: [user.id]))
+          @task_comment = Fabricate(:task_comment, task: @task)
+        end
+
+        it "renders them" do
+          render
+          assert_select "#comment-#{@task_comment.id}"
+        end
+      end
+    end
+
+    context "when no task_type" do
+      before do
+        @project = assign(:project, Fabricate(:project, category: @category))
+        @task = assign(:task, Fabricate(:task, project: @project))
+
+        @task.task_type.destroy
+        @task.reload
+      end
+
+      it "renders heading" do
+        render
+        assert_select ".task-heading", @task.heading
+      end
+    end
+
+    context "when task's user destroyed" do
+      before do
+        @project = assign(:project, Fabricate(:project, category: @category))
+        @task = assign(:task, Fabricate(:task, project: @project))
+
+        @task.user.destroy
+        @task.reload
+      end
+
+      it "renders default user name" do
+        render
+        assert_select ".task-user", User.destroyed_name
+      end
+    end
+
+    context "when comment's user destroyed" do
+      let(:user) { Fabricate(:user_worker) }
+
+      before do
+        @project = assign(:project, Fabricate(:project, category: @category))
+        @task = assign(:task, Fabricate(:task, project: @project))
+        @task_comment = Fabricate(:task_comment, task: @task, user: user)
+
+        @task_comment.user.destroy
+        @task_comment.reload
+      end
+
+      it "renders default user name" do
+        render
+        assert_select "#comment-#{@task_comment.id} .comment-user",
+                      User.destroyed_name
+      end
+    end
+
+    context "when task's assignee destroyed" do
+      let(:user) { Fabricate(:user_worker) }
+
+      before do
+        @project = assign(:project, Fabricate(:project, category: @category))
+        task = Fabricate(:task, project: @project, assignee_ids: [user.id])
+        @task = assign(:task, task)
+
+        user.destroy
         @task.reload
       end
 
@@ -178,6 +152,91 @@ RSpec.describe "tasks/show", type: :view do
         expect do
           render
         end.not_to raise_error
+      end
+    end
+
+    context "task's issue destroyed" do
+      before do
+        @project = assign(:project, Fabricate(:project, category: @category))
+        @issue = Fabricate(:issue, project: @project)
+        @task =
+          assign(:task, Fabricate(:task, project: @project, issue: @issue))
+
+        @issue.destroy
+        @task.reload
+      end
+
+      it "renders" do
+        expect do
+          render
+        end.not_to raise_error
+      end
+    end
+
+    # TODO: add specs for assignees, progressions, reviews, open, closed
+    context "task closed" do
+      context "with no reviews" do
+        before do
+          @project = assign(:project, Fabricate(:project, category: @category))
+          @task = assign(:task, Fabricate(:closed_task, project: @project))
+
+          @task.reload
+        end
+
+        it "renders" do
+          expect do
+            render
+          end.not_to raise_error
+        end
+      end
+    end
+
+    context "when task is open" do
+      before do
+        @project = assign(:project, Fabricate(:project, category: @category))
+        @task = assign(:task, Fabricate(:task, project: @project))
+        @review = Fabricate(:pending_review, task: @task)
+      end
+
+      it "renders approval link" do
+        render
+        expect(rendered)
+          .to have_link(nil, href: approve_task_review_path(@task, @review))
+      end
+
+      it "renders disapproval link" do
+        render
+        expect(rendered)
+          .to have_link(nil, href: disapprove_task_review_path(@task, @review))
+      end
+
+      it "renders new task connection link" do
+        render
+        expect(rendered)
+          .to have_link(nil, href: new_task_connection_path(@task))
+      end
+
+      it "renders close link" do
+        render
+        expect(rendered).to have_link(nil, href: close_task_path(@task))
+      end
+
+      it "renders edit link" do
+        render
+        url = edit_category_project_task_path(@category, @project, @task)
+        expect(rendered).to have_link(nil, href: url)
+      end
+    end
+
+    context "when task is closed" do
+      before do
+        @project = assign(:project, Fabricate(:project, category: @category))
+        @task = assign(:task, Fabricate(:closed_task, project: @project))
+      end
+
+      it "renders reopen link" do
+        render
+        expect(rendered).to have_link(nil, href: open_task_path(@task))
       end
     end
   end
