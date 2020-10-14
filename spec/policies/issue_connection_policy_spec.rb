fc8c4d3f305e688c12cdd67abea19949817fceef
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2020-10-14 09:47:10 -0700

    Fix IssueConnectionPolicy
    Don't need category and admin, missed user init

--- spec/policies/issue_connection_policy_spec.rb
+++ spec/policies/issue_connection_policy_spec.rb
@@ -3,44 +3,42 @@
 require "rails_helper"
 
 RSpec.describe IssueConnectionPolicy, type: :policy do
-  let(:admin) { Fabricate(:user_admin) }
-  let(:category) { Fabricate(:category) }
-
   subject { described_class }
 
   permissions :index?, :show? do
     %i[admin reviewer worker reporter].each do |employee_type|
       it "permits #{employee_type}" do
         user = Fabricate("user_#{employee_type}")
-        expect(subject).to permit(user, category)
+        expect(subject).to permit(user)
       end
     end
 
     it "blocks non-employees" do
       user = Fabricate(:user)
       user.employee_type = nil
-      expect(subject).not_to permit(user, category)
+      expect(subject).not_to permit(user)
     end
   end
 
   permissions :create?, :new?, :edit?, :update?, :destroy? do
     %i[admin reviewer].each do |employee_type|
       it "permits #{employee_type}" do
-        expect(subject).to permit(admin, category)
+        user = Fabricate("user_#{employee_type}")
+        expect(subject).to permit(user)
       end
     end
 
     %i[worker reporter].each do |employee_type|
       it "blocks #{employee_type}" do
         user = Fabricate("user_#{employee_type}")
-        expect(subject).not_to permit(user, category)
+        expect(subject).not_to permit(user)
       end
     end
 
     it "blocks non-employees" do
       user = Fabricate(:user)
       user.employee_type = nil
-      expect(subject).not_to permit(user, category)
+      expect(subject).not_to permit(user)
     end
   end
 end
