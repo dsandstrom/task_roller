dd144b0e805412099ef7064bd4db1ac0a14e21f5
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2020-10-13 18:26:53 -0700

    Update RollerConnection fabricators
    Allow auto setting source project from target

--- spec/fabricators/roller_connection_fabricator.rb
+++ spec/fabricators/roller_connection_fabricator.rb
@@ -1,11 +1,35 @@
 # frozen_string_literal: true
 
 Fabricator(:issue_connection) do
-  source { Fabricate(:issue) }
-  target { |attrs| Fabricate(:issue, project: attrs[:source].project) }
+  source do |attrs|
+    if attrs[:target].project
+      Fabricate(:issue, project: attrs[:target].project)
+    else
+      Fabricate(:issue)
+    end
+  end
+  target do |attrs|
+    if attrs[:source].project
+      Fabricate(:issue, project: attrs[:source].project)
+    else
+      Fabricate(:issue)
+    end
+  end
 end
 
 Fabricator(:task_connection) do
-  source { Fabricate(:task) }
-  target { |attrs| Fabricate(:task, project: attrs[:source].project) }
+  source do |attrs|
+    if attrs[:target].project
+      Fabricate(:task, project: attrs[:target].project)
+    else
+      Fabricate(:task)
+    end
+  end
+  target do |attrs|
+    if attrs[:source].project
+      Fabricate(:task, project: attrs[:source].project)
+    else
+      Fabricate(:task)
+    end
+  end
 end
