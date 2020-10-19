8e9e580c73e2cb94d7aa9aa265ece109710a6d97
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2020-10-18 21:58:16 -0700

    Authorize resolution index links

--- spec/views/resolutions/index.html.erb_spec.rb
+++ spec/views/resolutions/index.html.erb_spec.rb
@@ -3,19 +3,69 @@
 require "rails_helper"
 
 RSpec.describe "resolutions/index", type: :view do
-  context "when issue has resolutions" do
-    let(:issue) { assign(:issue, Fabricate(:issue)) }
-    let(:first_resolution) { Fabricate(:disapproved_resolution, issue: issue) }
-    let(:second_resolution) { Fabricate(:pending_resolution, issue: issue) }
+  context "for an admin" do
+    let(:admin) { Fabricate(:user_admin) }
 
-    before do
-      assign(:resolutions, [first_resolution, second_resolution])
+    before { enable_pundit(view, admin) }
+
+    context "when issue has resolutions" do
+      let(:issue) { assign(:issue, Fabricate(:issue)) }
+      let(:first_resolution) do
+        Fabricate(:disapproved_resolution, issue: issue)
+      end
+      let(:second_resolution) { Fabricate(:pending_resolution, issue: issue) }
+
+      before do
+        assign(:resolutions, [first_resolution, second_resolution])
+      end
+
+      it "renders a list of resolutions" do
+        render
+        assert_select "#resolution_#{first_resolution.id}"
+        assert_select "#resolution_#{second_resolution.id}"
+      end
+
+      it "renders destroy links" do
+        render
+        [first_resolution, second_resolution].each do |resolution|
+          url = issue_resolution_path(issue, resolution)
+          assert_select "a[data-method='delete'][href=\"#{url}\"]"
+        end
+      end
     end
+  end
 
-    it "renders a list of resolutions" do
-      render
-      assert_select "#resolution_#{first_resolution.id}"
-      assert_select "#resolution_#{second_resolution.id}"
+  %w[reviewer worker reporter].each do |employee_type|
+    context "for a #{employee_type}" do
+      let(:current_user) { Fabricate("user_#{employee_type}") }
+
+      before { enable_pundit(view, current_user) }
+
+      context "when issue has resolutions" do
+        let(:issue) { assign(:issue, Fabricate(:issue)) }
+        let(:first_resolution) do
+          Fabricate(:disapproved_resolution, issue: issue)
+        end
+        let(:second_resolution) { Fabricate(:pending_resolution, issue: issue) }
+
+        before do
+          assign(:resolutions, [first_resolution, second_resolution])
+        end
+
+        it "renders a list of resolutions" do
+          render
+          assert_select "#resolution_#{first_resolution.id}"
+          assert_select "#resolution_#{second_resolution.id}"
+        end
+
+        it "doesn't render destroy links" do
+          render
+          [first_resolution, second_resolution].each do |resolution|
+            url = issue_resolution_path(issue, resolution)
+            assert_select "a[data-method='delete'][href=\"#{url}\"]", count: 0
+          end
+        end
+      end
     end
   end
 end
--- spec/views/reviews/index.html.erb_spec.rb
+++ spec/views/reviews/index.html.erb_spec.rb
@@ -3,19 +3,51 @@
 require "rails_helper"
 
 RSpec.describe "reviews/index", type: :view do
-  context "when task has reviews" do
-    let(:task) { assign(:task, Fabricate(:task)) }
-    let(:first_review) { Fabricate(:disapproved_review, task: task) }
-    let(:second_review) { Fabricate(:pending_review, task: task) }
+  context "for an admin" do
+    let(:admin) { Fabricate(:user_admin) }
 
-    before do
-      assign(:reviews, [first_review, second_review])
+    before { enable_pundit(view, admin) }
+
+    context "when task has reviews" do
+      let(:task) { assign(:task, Fabricate(:task)) }
+      let(:first_review) { Fabricate(:disapproved_review, task: task) }
+      let(:second_review) { Fabricate(:pending_review, task: task) }
+
+      before do
+        assign(:reviews, [first_review, second_review])
+      end
+
+      it "renders a list of reviews" do
+        render
+        [first_review, second_review].each do |review|
+          assert_select "#review_#{review.id}"
+        end
+      end
     end
+  end
 
-    it "renders a list of reviews" do
-      render
-      assert_select "#review_#{first_review.id}"
-      assert_select "#review_#{second_review.id}"
+  %w[reviewer worker reporter].each do |employee_type|
+    context "for a #{employee_type}" do
+      let(:current_user) { Fabricate("user_#{employee_type}") }
+
+      before { enable_pundit(view, current_user) }
+
+      context "when task has reviews" do
+        let(:task) { assign(:task, Fabricate(:task)) }
+        let(:first_review) { Fabricate(:disapproved_review, task: task) }
+        let(:second_review) { Fabricate(:pending_review, task: task) }
+
+        before do
+          assign(:reviews, [first_review, second_review])
+        end
+
+        it "renders a list of reviews" do
+          render
+          [first_review, second_review].each do |review|
+            assert_select "#review_#{review.id}"
+          end
+        end
+      end
     end
   end
 end
