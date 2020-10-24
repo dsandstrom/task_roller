3690a100a24982b29d60483088698115d6f32581
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2020-10-23 21:22:43 -0700

    Add user nav to header helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,7 +7,20 @@ module UsersHelper
       if user.present?
         link = link_to_unless_current(user.name_or_email, user)
         concat content_tag(:h1, link)
+        concat user_nav(user)
       end
     end
   end
+
+  private
+
+    def user_nav(user)
+      content_tag :p, class: 'user-nav' do
+        concat link_to_unless_current('Reported Issues', user_issues_path(user))
+        concat ' '
+        concat divider
+        concat ' '
+        concat link_to_unless_current('User Tasks', user_tasks_path(user))
+      end
+    end
 end
