2fa9d94c6d2647f09dc75009eef99c5b4a5e19ca
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2020-10-23 19:03:21 -0700

    Authorize new project task link

--- spec/views/tasks/index.html.erb_spec.rb
+++ spec/views/tasks/index.html.erb_spec.rb
@@ -8,40 +8,6 @@ RSpec.describe "tasks/index", type: :view do
 
     before { enable_pundit(view, admin) }
 
-    context "when no category and project" do
-      let(:first_task) { Fabricate(:task) }
-      let(:second_task) { Fabricate(:task) }
-
-      before(:each) { assign(:tasks, [first_task, second_task]) }
-
-      it "renders a list of tasks" do
-        render
-        assert_select "#task-#{first_task.id}"
-        assert_select "#task-#{second_task.id}"
-      end
-    end
-
-    context "when only category" do
-      let(:category) { Fabricate(:category) }
-      let(:first_task) do
-        Fabricate(:task, project: Fabricate(:project, category: category))
-      end
-      let(:second_task) do
-        Fabricate(:task, project: Fabricate(:project, category: category))
-      end
-
-      before(:each) do
-        assign(:category, category)
-        assign(:tasks, [first_task, second_task])
-      end
-
-      it "renders a list of tasks" do
-        render
-        assert_select "#task-#{first_task.id}"
-        assert_select "#task-#{second_task.id}"
-      end
-    end
-
     context "when project" do
       let(:category) { Fabricate(:category) }
       let(:project) { Fabricate(:project, category: category) }
@@ -53,6 +19,12 @@ RSpec.describe "tasks/index", type: :view do
         assign(:category, category)
         assign(:project, project)
         assign(:tasks, [first_task, second_task])
+      end
+
+      it "renders new task link" do
+        render
+
+        expect(rendered).to have_link(nil, href: new_project_task_path(project))
       end
 
       it "renders a list of tasks" do
@@ -89,7 +61,7 @@ RSpec.describe "tasks/index", type: :view do
     end
   end
 
-  %w[reviewer worker reporter].each do |employee_type|
+  %w[reviewer].each do |employee_type|
     context "for a #{employee_type}" do
       let(:current_user) { Fabricate("user_#{employee_type}") }
 
@@ -108,6 +80,57 @@ RSpec.describe "tasks/index", type: :view do
           assign(:category, category)
           assign(:project, project)
           assign(:tasks, [first_task, second_task])
+        end
+
+        it "renders new task link" do
+          render
+
+          expect(rendered)
+            .to have_link(nil, href: new_project_task_path(project))
+        end
+
+        it "renders a list of tasks" do
+          render
+
+          assert_select "#task-#{first_task.id}"
+          first_url = edit_task_path(first_task)
+          expect(rendered).to have_link(nil, href: first_url)
+          assert_select "#task-#{second_task.id}"
+          second_url = edit_task_path(second_task)
+          expect(rendered).not_to have_link(nil, href: second_url)
+          show_url = issue_path(issue)
+          expect(rendered).to have_link(nil, href: show_url)
+        end
+      end
+    end
+  end
+
+  %w[worker reporter].each do |employee_type|
+    context "for a #{employee_type}" do
+      let(:current_user) { Fabricate("user_#{employee_type}") }
+
+      before { enable_pundit(view, current_user) }
+
+      context "when project" do
+        let(:category) { Fabricate(:category) }
+        let(:project) { Fabricate(:project, category: category) }
+        let(:first_task) do
+          Fabricate(:task, project: project, user: current_user)
+        end
+        let(:issue) { Fabricate(:issue, project: project) }
+        let(:second_task) { Fabricate(:task, project: project, issue: issue) }
+
+        before(:each) do
+          assign(:category, category)
+          assign(:project, project)
+          assign(:tasks, [first_task, second_task])
+        end
+
+        it "doesn't render new task link" do
+          render
+
+          expect(rendered)
+            .not_to have_link(nil, href: new_project_task_path(project))
         end
 
         it "renders a list of tasks" do
