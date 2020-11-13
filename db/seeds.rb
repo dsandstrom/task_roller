d4a1ff8f107e02ef6d934386654c06e4521745a4
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2020-11-12 16:27:24 -0800

    Update seeds
    Add duplicate, closed, reopened issues/tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -64,6 +64,14 @@ class Seeds # rubocop:disable Metrics/ClassLength
       rand(3..11).times { create_being_worked_issue(user) }
       rand(3..11).times { create_addressed_issue(user) }
       rand(3..11).times { create_resolved_issue(user) }
+      rand(3..11).times { create_closed_issue(user) }
+      rand(1..5).times { create_reopened_issue(user) }
+      rand(1..5).times { create_duplicate_issue(user) }
+    end
+
+    User.reviewers.each do |user|
+      rand(3..11).times { create_reopened_task(user) }
+      rand(1..5).times { create_duplicate_task(user) }
     end
   end
 
@@ -127,6 +135,32 @@ class Seeds # rubocop:disable Metrics/ClassLength
       issue
     end
 
+    def create_closed_issue(user)
+      issue = create_issue(user_id: user.id, closed: true)
+      issue.closures.create!(user_id: random_reviewer_id)
+      issue
+    end
+
+    def create_reopened_issue(user)
+      issue = create_issue(user_id: user.id, closed: false)
+      reviewer_id = random_reviewer_id
+      issue.closures.create!(user_id: reviewer_id)
+      issue.reopenings.create!(user_id: reviewer_id)
+      issue
+    end
+
+    def create_duplicate_issue(user)
+      duplicate_issue = Issue.all.sample
+      return unless duplicate_issue
+
+      issue = create_issue(user_id: user.id, closed: true,
+                           project_id: duplicate_issue.project_id)
+      IssueConnection.create!(source_id: issue.id,
+                              target_id: duplicate_issue.id,
+                              user_id: random_reviewer_id)
+      issue
+    end
+
     def create_open_task(issue, worker = nil)
       worker ||= User.workers.sample
       create_task(issue_id: issue.id, closed: false,
@@ -135,8 +169,26 @@ class Seeds # rubocop:disable Metrics/ClassLength
 
     def create_closed_task(issue, worker = nil)
       worker ||= User.workers.sample
-      create_task(issue_id: issue.id, closed: true,
-                  assignee_ids: [worker.id])
+      task = create_task(issue_id: issue.id, closed: true,
+                         assignee_ids: [worker.id])
+      task.closures.create!(user_id: random_reviewer_id)
+      task
+    end
+
+    def create_reopened_task(reviewer)
+      worker ||= User.workers.sample
+      task = create_task(closed: false, assignee_ids: [worker.id])
+      task.closures.create!(user_id: reviewer.id)
+      task.reopenings.create!(user_id: reviewer.id)
+      task
+    end
+
+    def create_duplicate_task(reviewer)
+      duplicate = Task.all.sample
+      task = create_task(closed: true, project_id: duplicate.project_id)
+      TaskConnection.create!(source_id: task.id, target_id: duplicate.id,
+                             user_id: reviewer.id)
+      task
     end
 
     def create_unassigned_task(issue)
@@ -228,6 +280,14 @@ class Seeds # rubocop:disable Metrics/ClassLength
 
     def random_user_id
       user_ids.sample
+    end
+
+    def reviewer_ids
+      @reviewer_ids ||= User.reviewers.ids
+    end
+
+    def random_reviewer_id
+      reviewer_ids.sample
     end
 end
 
