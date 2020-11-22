71659bfbd14ed9300ce04bb6dbe648fa865b8a09
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2020-11-22 12:41:14 -0800

    Update Category/Projet fabricators
    add unique names

--- spec/fabricators/category_fabricator.rb
+++ spec/fabricators/category_fabricator.rb
@@ -7,9 +7,11 @@ Fabricator(:category, alias: %i[visible_category external_category]) do
 end
 
 Fabricator(:invisible_category, from: :category) do
+  name { sequence(:categories) { |n| "Invisible Category Name #{n + 1}" } }
   visible false
 end
 
 Fabricator(:internal_category, from: :category) do
+  name { sequence(:categories) { |n| "Internal Category Name #{n + 1}" } }
   internal true
 end
--- spec/fabricators/project_fabricator.rb
+++ spec/fabricators/project_fabricator.rb
@@ -8,9 +8,11 @@ Fabricator(:project, alias: %i[visible_project external_project]) do
 end
 
 Fabricator(:invisible_project, from: :project) do
+  name { sequence(:projects) { |n| "Archived Project Name #{n + 1}" } }
   visible false
 end
 
 Fabricator(:internal_project, from: :project) do
+  name { sequence(:projects) { |n| "Internal Project Name #{n + 1}" } }
   internal true
 end
