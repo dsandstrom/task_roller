451da82fd13f2abe73966043400593a78ddc3a13
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2020-12-05 13:03:55 -0800

    Add auth to user edit pages
    Block destroy button, will bring back after adding papertrail
    Only show devise edit link if current user

--- spec/views/users/edit.html.erb_spec.rb
+++ spec/views/users/edit.html.erb_spec.rb
@@ -3,17 +3,22 @@
 require "rails_helper"
 
 RSpec.describe "users/edit", type: :view do
-  let(:user) { Fabricate(:user) }
+  context "for a reporter" do
+    let(:current_user) { Fabricate(:user_reporter) }
 
-  before(:each) { @user = assign(:user, user) }
+    before do
+      enable_can(view, current_user)
+      @user = assign(:user, current_user)
+    end
 
-  it "renders the edit user form" do
-    render
+    it "renders the edit user form" do
+      render
 
-    assert_select "form[action=?][method=?]", user_path(@user), "post" do
-      assert_select "input[name=?]", "user[name]"
-      assert_select "input[name=?][disabled]", "user[email]"
-      assert_select "input[name=?]", "user[employee_type]", count: 0
+      assert_select "form[action=?][method=?]", user_path(@user), "post" do
+        assert_select "input[name=?]", "user[name]"
+        assert_select "input[name=?][disabled]", "user[email]"
+        assert_select "input[name=?]", "user[employee_type]", count: 0
+      end
     end
   end
 end
