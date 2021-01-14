b2867ccc278a3b6ad4f56a469d857ec2aaf3a8b0
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2021-01-13 17:32:33 -0800

    Add StaticController spec

---
+++ spec/controllers/static_controller_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe StaticController, type: :controller do
+  describe "GET #unauthorized" do
+    User::VALID_EMPLOYEE_TYPES.each do |employee_type|
+      context "for a #{employee_type}" do
+        let(:current_user) { Fabricate("user_#{employee_type.downcase}") }
+
+        before { sign_in(current_user) }
+
+        it "returns a success response" do
+          get :unauthorized
+          expect(response).to be_successful
+        end
+      end
+    end
+  end
+end
