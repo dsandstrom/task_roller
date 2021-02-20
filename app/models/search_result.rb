cfc05fcc320759fdb41999493b6b470c628c5a91
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2021-02-19 22:00:49 -0800

    Add instance methods to SearchResult
    helpers needed for partials

--- app/models/search_result.rb
+++ app/models/search_result.rb
@@ -15,4 +15,27 @@ class SearchResult < ApplicationRecord
   belongs_to :issue_type, foreign_key: :type_id
   has_many :task_assignees, foreign_key: :task_id
   has_many :assignees, through: :task_assignees
+
+  # INSTANCE
+
+  def issue?
+    class_name == 'Issue'
+  end
+
+  def task?
+    class_name == 'Task'
+  end
+
+  def heading
+    @heading ||=
+      ("#{class_name} \##{id}: #{short_summary}" if id && summary.present?)
+  end
+
+  def short_summary
+    @short_summary ||= summary&.truncate(70)
+  end
+
+  def status
+    # TODO: hardcode status in issues/tasks, and update with callbacks
+  end
 end
--- spec/models/search_result_spec.rb
+++ spec/models/search_result_spec.rb
@@ -30,4 +30,112 @@ RSpec.describe SearchResult, type: :model do
 
   it { is_expected.to have_many(:task_assignees) }
   it { is_expected.to have_many(:assignees) }
+
+  # CLASS
+
+  # INSTANCE
+
+  describe "#issue?" do
+    context "when class_name is 'Issue'" do
+      before { subject.class_name = "Issue" }
+
+      it "returns true" do
+        expect(subject.issue?).to eq(true)
+      end
+    end
+
+    context "when class_name is 'Task'" do
+      before { subject.class_name = "Task" }
+
+      it "returns false" do
+        expect(subject.issue?).to eq(false)
+      end
+    end
+
+    context "when class_name is something else" do
+      before { subject.class_name = "Something Else" }
+
+      it "returns false" do
+        expect(subject.issue?).to eq(false)
+      end
+    end
+  end
+
+  describe "#task?" do
+    context "when class_name is 'Issue'" do
+      before { subject.class_name = "Issue" }
+
+      it "returns false" do
+        expect(subject.task?).to eq(false)
+      end
+    end
+
+    context "when class_name is 'Task'" do
+      before { subject.class_name = "Task" }
+
+      it "returns true" do
+        expect(subject.task?).to eq(true)
+      end
+    end
+
+    context "when class_name is something else" do
+      before { subject.class_name = "Something Else" }
+
+      it "returns false" do
+        expect(subject.task?).to eq(false)
+      end
+    end
+  end
+
+  describe "#heading" do
+    let(:task) { Fabricate(:task) }
+
+    before do
+      subject.class_name = "Task"
+      subject.id = task.id
+      subject.summary = task.summary
+    end
+
+    context "when a summary" do
+      it "returns class_name and short_summary" do
+        expect(subject.heading)
+          .to eq("Task \##{subject.id}: #{subject.summary}")
+      end
+    end
+
+    context "when summary is blank" do
+      before { subject.summary = "" }
+
+      it "returns nil" do
+        expect(subject.heading).to be_nil
+      end
+    end
+  end
+
+  describe "#short_summary" do
+    let(:short_summary_length) { 70 }
+
+    context "when summary is short" do
+      before { subject.summary = "short" }
+
+      it "lets it be" do
+        expect(subject.short_summary).to eq("short")
+      end
+    end
+
+    context "when summary is 1 too long" do
+      before { subject.summary = "#{'a' * short_summary_length}b" }
+
+      it "truncates it" do
+        expect(subject.short_summary)
+          .to eq("#{'a' * (short_summary_length - 3)}...")
+      end
+    end
+  end
+
+  describe "#status" do
+    it "returns only nil for now" do
+      expect(subject.status).to be_nil
+    end
+  end
 end
