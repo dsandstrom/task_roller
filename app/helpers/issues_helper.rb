36d3db2bfa208bffa35d887914f0d610d4b90461
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2021-02-25 17:56:49 -0800

    Add issue status dropdown
    Will use instead of sidebar links

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -20,7 +20,8 @@ module IssuesHelper
   def project_and_issue_tags(issue)
     project = issue.project
     tags = [project_invisible_tag(project), project_internal_tag(project),
-            issue_type_tag(issue.issue_type), issue_status_tag(issue)].compact
+            issue_type_tag(issue.issue_type),
+            issue_status_button(issue), issue_status_dropdown(issue)].compact
 
     content_tag :div, class: 'project-tags issue-tags' do
       safe_join(tags)
@@ -125,5 +126,95 @@ module IssuesHelper
 
       content_tag :span, value.titleize,
                   class: "status-tag roller-type-color-#{color}"
+    end
+
+    def issue_status_button(issue)
+      value = issue.status
+      return unless value
+
+      option = Issue::STATUS_OPTIONS[value.parameterize.underscore.to_sym]
+      return unless option
+
+      color = option[:color]
+      return unless color
+
+      klass = "status-tag roller-type-color-#{color}"
+      parts = [content_tag(:span, value.titleize, class: 'status-value')]
+      dropdown = issue_status_dropdown(issue)
+      if dropdown
+        parts << issue_status_dropdown_link
+        klass += ' status-button'
+      end
+      content_tag :span, class: klass do
+        safe_join(parts)
+      end
+    end
+
+    def issue_status_dropdown_link
+      link_to '', 'javascript:void(0)',
+              class: 'dropdown-link status-dropdown-link',
+              title: 'Change Status'
+    end
+
+    def issue_status_dropdown(issue)
+      options = { class: 'dropdown-menu status-dropdown',
+                  data: { link: 'status-dropdown-link' } }
+
+      containers = [issue_status_user_actions(issue),
+                    issue_status_reviewer_actions(issue)].compact
+      return unless containers.any?
+
+      content_tag :div, options do
+        safe_join(containers)
+      end
+    end
+
+    def issue_status_reviewer_actions(issue)
+      links = []
+      if issue.open?
+        if can?(:create, new_issue_connection(issue))
+          links << link_to('mark as duplicate and close',
+                           new_issue_connection_path(issue))
+        end
+        if can?(:create, new_issue_closure(issue))
+          links << link_to('mark invalid and close', issue_closures_path(issue),
+                           method: :post)
+        end
+      end
+      if issue.closed? && can?(:create, new_issue_reopening(issue))
+        links << link_to('reopen issue', issue_reopenings_path(issue),
+                         method: :post)
+      end
+      if can?(:move, issue)
+        links << link_to('move to a different project',
+                         new_issue_move_path(issue))
+      end
+      return if links.none?
+
+      content_tag :div, class: 'dropdown-menu-container status-admin-actions' do
+        concat content_tag :span, 'Review Actions', class: 'dropdown-menu-title'
+        concat safe_join(links)
+      end
+    end
+
+    def issue_status_user_actions(issue)
+      links = []
+      if issue.status == 'addressed' && can?(:create, new_resolution(issue))
+        links << link_to('report issue was not fixed and reopen',
+                         disapprove_issue_resolutions_path(issue),
+                         method: :post)
+      elsif issue.status == 'open' || issue.status == 'being_worked_on'
+        if can?(:create, new_resolution(issue))
+          links << link_to('mark resolved and close',
+                           approve_issue_resolutions_path(issue),
+                           method: :post, class: 'destroy-link')
+        end
+      end
+      return if links.none?
+
+      content_tag :div, class: 'dropdown-menu-container status-user-actions' do
+        concat content_tag :span, 'User Actions', class: 'dropdown-menu-title'
+        concat safe_join(links)
+      end
     end
 end
