92ca45abc240d5b5ddb8ad92b25db9200a2f58ed
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2021-03-15 15:06:42 -0700

    Refactor

--- app/mailers/issue_mailer.rb
+++ app/mailers/issue_mailer.rb
@@ -1,15 +1,22 @@
 # frozen_string_literal: true
 
 class IssueMailer < ApplicationMailer
+  attr_accessor :issue, :user, :old_status, :new_status
+
   def status_change
-    @issue = params[:issue]
-    @user = params[:user]
-    @old_status = params[:old_status]
-    @new_status = params[:new_status]
+    set_instance_variables
 
     options = { to: @user.email }
     options[:subject] = "Task Roller Update for Issue##{@issue.id}"
 
     mail(options)
   end
+
+  private
+
+    def set_instance_variables
+      %i[issue user old_status new_status].each do |attribute|
+        send("#{attribute}=", params[attribute])
+      end
+    end
 end
