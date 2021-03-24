467f4c17fc0882d0fca998a16c7eba84fbc0eca3
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2021-03-23 21:19:28 -0700

    Add todo

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 # TODO: search categories and projects
+# TODO: allow searching by id
 
 class SearchesController < ApplicationController
   before_action :authorize_search
