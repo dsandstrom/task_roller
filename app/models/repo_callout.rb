13192069f912865a4150c05bb22e01eae1385538
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2021-09-17 01:09:27 -0700

    Add RepoCallout#process_commit_message
    Generate action and task from commit_message

--- app/models/repo_callout.rb
+++ app/models/repo_callout.rb
@@ -2,6 +2,11 @@
 
 class RepoCallout < ApplicationRecord
   ACTION_OPTIONS = %w[start pause complete].freeze
+  MESSAGE_REGEX = /
+    (starts?|fix(?:e[ds])?|(?:pause|close|complete)[ds]?)\s
+    (?:task)?\s?\#?
+    (\d+)
+  /ix.freeze
 
   validates :task_id, presence: true
   validates :action, presence: true, inclusion: { in: ACTION_OPTIONS }
@@ -11,4 +16,42 @@ class RepoCallout < ApplicationRecord
 
   belongs_to :task
   belongs_to :user, optional: true
+
+  def process_commit_message
+    return if action && task_id
+
+    matches = commit_message.match(MESSAGE_REGEX)
+    return unless matches && matches.size == 3
+
+    action = generate_action(matches[1])
+    task = Task.find_by(id: matches[2].to_i)
+    return unless action && task
+
+    self.action = action
+    self.task_id = task.id
+  end
+
+  private
+
+    def generate_action(part)
+      if look_like_start?(part)
+        'start'
+      elsif look_like_pause?(part)
+        'pause'
+      elsif look_like_complete?(part)
+        'complete'
+      end
+    end
+
+    def look_like_start?(part)
+      part.match?(/start/i)
+    end
+
+    def look_like_pause?(part)
+      part.match?(/pause/i)
+    end
+
+    def look_like_complete?(part)
+      part.match?(/fix|close|complete/i)
+    end
 end
--- spec/models/repo_callout_spec.rb
+++ spec/models/repo_callout_spec.rb
@@ -33,4 +33,244 @@ RSpec.describe RepoCallout, type: :model do
 
   it { is_expected.to belong_to(:user) }
   it { is_expected.to belong_to(:task) }
+
+  describe "#process_commit_message" do
+    context "when no action and task_id" do
+      before do
+        subject.action = nil
+        subject.task_id = nil
+      end
+
+      context "when no commit message" do
+        before { subject.commit_message = "" }
+
+        it "doesn't change action" do
+          expect do
+            subject.process_commit_message
+          end.not_to change(subject, :action)
+        end
+
+        it "doesn't change task_id" do
+          expect do
+            subject.process_commit_message
+          end.not_to change(subject, :task_id)
+        end
+      end
+
+      context "when commit_message" do
+        context "is 'Starts Task#id'" do
+          before { subject.commit_message = "Starts Task##{task.id}" }
+
+          it "changes action" do
+            expect do
+              subject.process_commit_message
+            end.to change(subject, :action).to("start")
+          end
+
+          it "changes task_id" do
+            expect do
+              subject.process_commit_message
+            end.to change(subject, :task_id).to(task.id)
+          end
+        end
+
+        context "is 'start Task#id'" do
+          before { subject.commit_message = "start Task##{task.id}" }
+
+          it "changes action" do
+            expect do
+              subject.process_commit_message
+            end.to change(subject, :action).to("start")
+          end
+
+          it "changes task_id" do
+            expect do
+              subject.process_commit_message
+            end.to change(subject, :task_id).to(task.id)
+          end
+        end
+
+        context "is 'Pauses Task#id'" do
+          before { subject.commit_message = "Pauses Task##{task.id}" }
+
+          it "changes action" do
+            expect do
+              subject.process_commit_message
+            end.to change(subject, :action).to("pause")
+          end
+
+          it "changes task_id" do
+            expect do
+              subject.process_commit_message
+            end.to change(subject, :task_id).to(task.id)
+          end
+        end
+
+        context "is 'Pause Task#id'" do
+          before { subject.commit_message = "Pause Task##{task.id}" }
+
+          it "changes action" do
+            expect do
+              subject.process_commit_message
+            end.to change(subject, :action).to("pause")
+          end
+
+          it "changes task_id" do
+            expect do
+              subject.process_commit_message
+            end.to change(subject, :task_id).to(task.id)
+          end
+        end
+
+        context "is 'Paused Task#id'" do
+          before { subject.commit_message = "Paused Task##{task.id}" }
+
+          it "changes action" do
+            expect do
+              subject.process_commit_message
+            end.to change(subject, :action).to("pause")
+          end
+
+          it "changes task_id" do
+            expect do
+              subject.process_commit_message
+            end.to change(subject, :task_id).to(task.id)
+          end
+        end
+
+        context "is 'Fixed Task#id'" do
+          before { subject.commit_message = "Fixes Task##{task.id}" }
+
+          it "changes action" do
+            expect do
+              subject.process_commit_message
+            end.to change(subject, :action).to("complete")
+          end
+
+          it "changes task_id" do
+            expect do
+              subject.process_commit_message
+            end.to change(subject, :task_id).to(task.id)
+          end
+        end
+
+        context "is 'Fix Task#id'" do
+          before { subject.commit_message = "Fix Task##{task.id}" }
+
+          it "changes action" do
+            expect do
+              subject.process_commit_message
+            end.to change(subject, :action).to("complete")
+          end
+
+          it "changes task_id" do
+            expect do
+              subject.process_commit_message
+            end.to change(subject, :task_id).to(task.id)
+          end
+        end
+
+        context "is 'Fixes Task#id'" do
+          before { subject.commit_message = "Fixes Task##{task.id}" }
+
+          it "changes action" do
+            expect do
+              subject.process_commit_message
+            end.to change(subject, :action).to("complete")
+          end
+
+          it "changes task_id" do
+            expect do
+              subject.process_commit_message
+            end.to change(subject, :task_id).to(task.id)
+          end
+        end
+
+        context "is 'Closes Task#id'" do
+          before { subject.commit_message = "Closes Task##{task.id}" }
+
+          it "changes action" do
+            expect do
+              subject.process_commit_message
+            end.to change(subject, :action).to("complete")
+          end
+
+          it "changes task_id" do
+            expect do
+              subject.process_commit_message
+            end.to change(subject, :task_id).to(task.id)
+          end
+        end
+
+        context "is 'Completes Task#id'" do
+          before { subject.commit_message = "Completes Task##{task.id}" }
+
+          it "changes action" do
+            expect do
+              subject.process_commit_message
+            end.to change(subject, :action).to("complete")
+          end
+
+          it "changes task_id" do
+            expect do
+              subject.process_commit_message
+            end.to change(subject, :task_id).to(task.id)
+          end
+        end
+
+        context "is 'Starts Task#'" do
+          before { subject.commit_message = "Starts Task#" }
+
+          it "doesn't change action" do
+            expect do
+              subject.process_commit_message
+            end.not_to change(subject, :action)
+          end
+
+          it "doesn't change task_id" do
+            expect do
+              subject.process_commit_message
+            end.not_to change(subject, :task_id)
+          end
+        end
+
+        context "is 'Task#id'" do
+          before { subject.commit_message = "Task##{task.id}" }
+
+          it "doesn't change action" do
+            expect do
+              subject.process_commit_message
+            end.not_to change(subject, :action)
+          end
+
+          it "doesn't change task_id" do
+            expect do
+              subject.process_commit_message
+            end.not_to change(subject, :task_id)
+          end
+        end
+      end
+    end
+
+    context "when action and task_id" do
+      before do
+        subject.action = "complete"
+        subject.task_id = Fabricate(:task).id
+      end
+
+      before { subject.commit_message = "Starts Task##{task.id}" }
+
+      it "doesn't change action" do
+        expect do
+          subject.process_commit_message
+        end.not_to change(subject, :action)
+      end
+
+      it "doesn't change task_id" do
+        expect do
+          subject.process_commit_message
+        end.not_to change(subject, :task_id)
+      end
+    end
+  end
 end
