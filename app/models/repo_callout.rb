a96e97462a065e295e3a8417b79442c9406a69ee
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2021-09-17 18:44:44 -0700

    Add todo

--- app/models/repo_callout.rb
+++ app/models/repo_callout.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# TODO: add 'progresses' action to add progression
+
 class RepoCallout < ApplicationRecord
   ACTION_OPTIONS = %w[start pause complete].freeze
   MESSAGE_REGEX = /
