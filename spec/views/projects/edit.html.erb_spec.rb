3a60d99dae7aac7a10520f7315aee54a4f3aa0a2
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2022-01-19 19:49:12 -0800

    Update project form
    When category is internal/invisible, no reason to show options in the
    project

--- spec/views/projects/edit.html.erb_spec.rb
+++ spec/views/projects/edit.html.erb_spec.rb
@@ -26,5 +26,50 @@ RSpec.describe "projects/edit", type: :view do
         assert_select "select[name=?]", "project[category_id]"
       end
     end
+
+    context "when category is internal" do
+      let(:category) { Fabricate(:internal_category) }
+
+      it "renders the edit project form" do
+        render
+
+        assert_select "form[action=?][method=?]", form_path, "post" do
+          assert_select "input[name=?]", "project[name]"
+          assert_select "input[name=?]", "project[visible]"
+          assert_select "input[name=?]", "project[internal]", count: 0
+          assert_select "select[name=?]", "project[category_id]"
+        end
+      end
+    end
+
+    context "when category is invisible" do
+      let(:category) { Fabricate(:invisible_category) }
+
+      it "renders the edit project form" do
+        render
+
+        assert_select "form[action=?][method=?]", form_path, "post" do
+          assert_select "input[name=?]", "project[name]"
+          assert_select "input[name=?]", "project[visible]", count: 0
+          assert_select "input[name=?]", "project[internal]"
+          assert_select "select[name=?]", "project[category_id]"
+        end
+      end
+    end
+
+    context "when category is internal & invisible" do
+      let(:category) { Fabricate(:invisible_category, internal: true) }
+
+      it "renders the edit project form" do
+        render
+
+        assert_select "form[action=?][method=?]", form_path, "post" do
+          assert_select "input[name=?]", "project[name]"
+          assert_select "input[name=?]", "project[visible]", count: 0
+          assert_select "input[name=?]", "project[internal]", count: 0
+          assert_select "select[name=?]", "project[category_id]"
+        end
+      end
+    end
   end
 end
--- spec/views/projects/new.html.erb_spec.rb
+++ spec/views/projects/new.html.erb_spec.rb
@@ -21,4 +21,49 @@ RSpec.describe "projects/new", type: :view do
       assert_select "input[name=?]", "project[category_id]", count: 0
     end
   end
+
+  context "when category is internal" do
+    let(:category) { Fabricate(:internal_category) }
+
+    it "renders the edit project form" do
+      render
+
+      assert_select "form[action=?][method=?]", path, "post" do
+        assert_select "input[name=?]", "project[name]"
+        assert_select "input[name=?]", "project[visible]"
+        assert_select "input[name=?]", "project[internal]", count: 0
+        assert_select "select[name=?]", "project[category_id]"
+      end
+    end
+  end
+
+  context "when category is invisible" do
+    let(:category) { Fabricate(:invisible_category) }
+
+    it "renders the edit project form" do
+      render
+
+      assert_select "form[action=?][method=?]", path, "post" do
+        assert_select "input[name=?]", "project[name]"
+        assert_select "input[name=?]", "project[visible]", count: 0
+        assert_select "input[name=?]", "project[internal]"
+        assert_select "select[name=?]", "project[category_id]"
+      end
+    end
+  end
+
+  context "when category is internal & invisible" do
+    let(:category) { Fabricate(:invisible_category, internal: true) }
+
+    it "renders the edit project form" do
+      render
+
+      assert_select "form[action=?][method=?]", path, "post" do
+        assert_select "input[name=?]", "project[name]"
+        assert_select "input[name=?]", "project[visible]", count: 0
+        assert_select "input[name=?]", "project[internal]", count: 0
+        assert_select "select[name=?]", "project[category_id]"
+      end
+    end
+  end
 end
