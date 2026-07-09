de775d3d7c37e9b587812920e9eb0e66c1e26e4e
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2026-07-08 18:45:08 -0700

    Fix rubocop error

--- config/boot.rb
+++ config/boot.rb
@@ -3,5 +3,7 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require "logger" # Fix concurrent-ruby removing logger dependency which Rails itself does not have
+# Fix concurrent-ruby removing logger dependency which Rails itself does not
+# have
+require 'logger'
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
