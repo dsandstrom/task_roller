ca92d51a266864bb04770b6ba08347b8d689af29
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2026-07-13 16:25:05 -0700

    App:update environments
    Keep action_mailer and active_job configs

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+require "active_support/core_ext/integer/time"
 
 # The test environment is used exclusively to run your application's
 # test suite. You never need to work with it otherwise. Remember that
@@ -6,8 +6,7 @@
 # and recreated between test runs. Don't rely on the data there!
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in
-  # config/application.rb.
+  # Settings specified here will take precedence over those in config/application.rb.
 
   config.cache_classes = false
   config.action_view.cache_template_loading = true
@@ -47,8 +46,17 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  # Raise exceptions for disallowed deprecations.
+  config.active_support.disallowed_deprecation = :raise
+
+  # Tell Active Support which deprecation messages to disallow.
+  config.active_support.disallowed_deprecation_warnings = []
+
   # Raises error for missing translations.
-  # config.action_view.raise_on_missing_translations = true
+  # config.i18n.raise_on_missing_translations = true
+
+  # Annotate rendered view with file names.
+  # config.action_view.annotate_rendered_view_with_filenames = true
 
   config.action_mailer.default_url_options = { host: 'test.loc', port: 80 }
 
