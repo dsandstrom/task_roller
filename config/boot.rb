59637083c341a840e6fd7fdec430f2d429e6336d
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2026-07-13 16:10:22 -0700

    App:update config/application and boot

--- config/boot.rb
+++ config/boot.rb
@@ -1,9 +1,4 @@
-# frozen_string_literal: true
-
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-require 'bundler/setup' # Set up gems listed in the Gemfile.
-# Fix concurrent-ruby removing logger dependency which Rails itself does not
-# have
-require 'logger'
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+require "bundler/setup" # Set up gems listed in the Gemfile.
+require "bootsnap/setup" # Speed up boot time by caching expensive operations.
