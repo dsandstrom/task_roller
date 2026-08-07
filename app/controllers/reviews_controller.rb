e0acaf2bb15ab0f485216c9415f4c9a58cc329d5
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2026-08-07 00:51:30 -0700

    Change review notifications

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class ReviewsController < ApplicationController
   load_and_authorize_resource :task
   load_and_authorize_resource through: :task
@@ -12,7 +10,7 @@ class ReviewsController < ApplicationController
     if @review.save
       @task.finish?
       @task.update_status
-      redirect_back_or_to(@task, notice: 'Review was successfully created.')
+      redirect_back_or_to(@task, notice: 'Task marked ready for review.')
     else
       render :new
     end
@@ -21,13 +19,13 @@ class ReviewsController < ApplicationController
   def destroy
     @review.destroy
     @task.update_status
-    redirect_back_or_to(@task, notice: 'Review was successfully destroyed.')
+    redirect_back_or_to(@task, notice: 'Review was canceled.')
   end
 
   def approve
     if @review.approve(current_user)
       @review.subscribe_user
-      redirect_back_or_to(@task, notice: 'Review was successfully updated.')
+      redirect_back_or_to(@task, notice: 'Task was approved.')
     else
       render :edit
     end
@@ -36,7 +34,7 @@ class ReviewsController < ApplicationController
   def disapprove
     if @review.disapprove(current_user)
       @review.subscribe_user
-      redirect_back_or_to(@task, notice: 'Review was successfully updated.')
+      redirect_back_or_to(@task, notice: 'Task was disapproved.')
     else
       render :edit
     end
