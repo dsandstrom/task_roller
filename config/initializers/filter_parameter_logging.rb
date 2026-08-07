c3e764bc855a27a77d8aca5028efbcbdf007084d
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2026-08-06 23:29:21 -0700

    Run rails app:update

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -6,5 +6,6 @@
 # See the ActiveSupport::ParameterFilter documentation for supported notations
 # and behaviors.
 Rails.application.config.filter_parameters += %i[
-  passw secret token _key crypt salt certificate otp ssn
+  passw email secret token _key crypt salt certificate otp
+  ssn
 ]
