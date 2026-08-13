696c8c815239cda465745392a626d6d0d17b217c
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2026-08-13 07:53:40 -0700

    Fix webhooks controller spec
    Use unprocessible_content

--- spec/controllers/api/v1/webhooks_controller_spec.rb
+++ spec/controllers/api/v1/webhooks_controller_spec.rb
@@ -364,7 +364,7 @@ RSpec.describe Api::V1::WebhooksController, type: :controller do
 
           it "returns an error response" do
             post :github, params: issue_open_params
-            expect(response).to have_http_status(:unprocessable_entity)
+            expect(response).to have_http_status(:unprocessable_content)
           end
         end
 
@@ -385,7 +385,7 @@ RSpec.describe Api::V1::WebhooksController, type: :controller do
 
           it "returns an error response" do
             post :github, params: issue_open_params
-            expect(response).to have_http_status(:unprocessable_entity)
+            expect(response).to have_http_status(:unprocessable_content)
           end
         end
 
