585f2c626640b81f1470a3454ea5f637f5797b6e
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2026-08-13 14:02:35 -0700

    Fix previews_path config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -88,5 +88,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.preview_path = Rails.root.join('spec/mailers/previews')
+  config.action_mailer.preview_paths =
+    [Rails.root.join('spec/mailers/previews')]
 end
