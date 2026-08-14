ccc2dc077c58a85c37549baa802f1057c81126f7
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2026-08-14 10:59:34 -0700

    Remove todos
    Not sure what the first one was for anymore and decided not to do the
    second
    
    Fixes Task#59

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,3 @@
-# FIXME: when same task on page, issue gets added to first
-# TODO: show description in partial?
-
 class TasksController < ApplicationController
   load_and_authorize_resource :project, only: %i[new create destroy]
   load_and_authorize_resource through: :project, only: %i[new create destroy]
