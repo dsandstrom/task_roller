885e5c9c521f7553a0e2becf01e6dcccbd381edd
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2026-08-25 13:47:27 -0700

    Add TaskSubscriberNotifierJob
    Copy logic form Task#notify_subscriber

---
+++ app/jobs/task_subscriber_notifier_job.rb
@@ -0,0 +1,27 @@
+class TaskSubscriberNotifierJob < ApplicationJob
+  queue_as :default
+
+  def perform(task, subscriber, options)
+    options.merge!(user: subscriber)
+    notification = find_or_create_notification(task, subscriber, options)
+
+    notification.send_email
+  end
+
+  private
+
+    def find_or_create_notification(task, subscriber, options)
+      if options[:event] == 'status'
+        notification = task.notifications.find_by(user_id: subscriber.id,
+                                                  event: 'status')
+      end
+
+      if notification
+        notification.update(options)
+      else
+        notification = task.notifications.create!(options)
+      end
+
+      notification
+    end
+end
