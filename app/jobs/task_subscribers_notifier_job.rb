e4db1814c9ff156f26c9c3e5dba2d8331920f927
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2026-08-25 14:32:20 -0700

    Add TaskSubscribersNotifierJob
    Copy logic from Task#notify_subscribers

---
+++ app/jobs/task_subscribers_notifier_job.rb
@@ -0,0 +1,24 @@
+class TaskSubscribersNotifierJob < ApplicationJob
+  queue_as :default
+
+  attr_accessor :task
+
+  def perform(task, options)
+    self.task = task
+
+    current_user = options.delete(:current_user)
+    subscribers_except(current_user).each do |subscriber|
+      TaskSubscriberNotifierJob.perform_later(task, subscriber, options)
+    end
+  end
+
+  private
+
+    def subscribers_except(ignored_user = nil)
+      if ignored_user
+        task.subscribers.where.not(id: ignored_user.id)
+      else
+        task.subscribers
+      end
+    end
+end
---
+++ spec/jobs/task_subscribers_notifier_job_spec.rb
@@ -0,0 +1,64 @@
+require "rails_helper"
+
+RSpec.describe TaskSubscribersNotifierJob, type: :job do
+  include ActiveJob::TestHelper
+
+  let(:task) { Fabricate(:task) }
+  let(:current_user) { Fabricate(:user_reviewer) }
+
+  subject { described_class }
+
+  describe "#perform" do
+    let(:options) { { event: "status", details: "unassigned,assigned" } }
+
+    context "for an task with subscribers" do
+      let(:subscriber) { Fabricate(:user) }
+
+      context "and options has a current_user" do
+        let(:bulk_options) { options.merge(current_user: current_user) }
+
+        before do
+          Fabricate(:task_subscription, task: task, user: current_user)
+          Fabricate(:task_subscription, task: task, user: subscriber)
+        end
+
+        it "enqueues one TaskSubscriberNotifierJob" do
+          subject.perform_now task, bulk_options
+
+          expect(TaskSubscriberNotifierJob)
+            .to have_been_enqueued.exactly(:once)
+          expect(TaskSubscriberNotifierJob)
+            .to have_been_enqueued.with(task, subscriber, options)
+        end
+      end
+
+      context "and options doesn't have a current_user" do
+        let(:bulk_options) { options }
+
+        before do
+          Fabricate(:task_subscription, task: task, user: current_user)
+          Fabricate(:task_subscription, task: task, user: subscriber)
+        end
+
+        it "enqueues one TaskSubscriberNotifierJob" do
+          subject.perform_now task, bulk_options
+
+          expect(TaskSubscriberNotifierJob)
+            .to have_been_enqueued.exactly(:twice)
+          expect(TaskSubscriberNotifierJob)
+            .to have_been_enqueued.with(task, subscriber, options)
+          expect(TaskSubscriberNotifierJob)
+            .to have_been_enqueued.with(task, current_user, options)
+        end
+      end
+    end
+
+    context "for an task without subscribers" do
+      it "doesn't enqueue a TaskSubscriberNotifierJob" do
+        subject.perform_now task, options
+
+        expect(TaskSubscriberNotifierJob).not_to have_been_enqueued
+      end
+    end
+  end
+end
