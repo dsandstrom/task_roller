dad4d8add73b2b96d6ab3bbc2aacebba9ffdb30a
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2026-08-30 15:49:17 -0700

    Update RepositionCategoriesController spec
    Add examples for json requests

--- spec/controllers/reposition_categories_controller_spec.rb
+++ spec/controllers/reposition_categories_controller_spec.rb
@@ -36,38 +36,82 @@ RSpec.describe RepositionCategoriesController, type: :controller do
         before { sign_in(Fabricate("user_#{employee_type}")) }
 
         context "with valid params" do
-          it "updates the requested category's position" do
-            category = Fabricate(:category)
-            expect do
+          context "for html request" do
+            it "updates the requested category's position" do
+              category = Fabricate(:category)
+              expect do
+                put :update, params: { id: category.to_param,
+                                       category: valid_attributes }
+                category.reload
+              end.to change(category, :position).from(2).to(1)
+            end
+
+            it "redirects to the category list" do
+              category = Fabricate(:category)
               put :update, params: { id: category.to_param,
                                      category: valid_attributes }
-              category.reload
-            end.to change(category, :position).from(2).to(1)
+              expect(response).to redirect_to(reposition_categories_path)
+            end
           end
 
-          it "redirects to the category list" do
-            category = Fabricate(:category)
-            put :update, params: { id: category.to_param,
-                                   category: valid_attributes }
-            expect(response).to redirect_to(reposition_categories_path)
+          context "for json request" do
+            it "updates the requested category's position" do
+              category = Fabricate(:category)
+              expect do
+                put :update, params: { id: category.to_param,
+                                       category: valid_attributes },
+                             as: :json
+                category.reload
+              end.to change(category, :position).from(2).to(1)
+            end
+
+            it "responds with ok" do
+              category = Fabricate(:category)
+              put :update, params: { id: category.to_param,
+                                     category: valid_attributes },
+                           as: :json
+              expect(response).to have_http_status(:ok)
+            end
           end
         end
 
         context "with invalid params" do
-          it "doesn't update the requested category" do
-            category = Fabricate(:category)
-            expect do
+          context "for html request" do
+            it "doesn't update the requested category" do
+              category = Fabricate(:category)
+              expect do
+                put :update, params: { id: category.to_param,
+                                       category: invalid_attributes }
+                category.reload
+              end.not_to change(category, :position)
+            end
+
+            it "redirects to the category list" do
+              category = Fabricate(:category)
               put :update, params: { id: category.to_param,
                                      category: invalid_attributes }
-              category.reload
-            end.not_to change(category, :position)
+              expect(response).to redirect_to(reposition_categories_path)
+            end
           end
 
-          it "redirects to the category list" do
-            category = Fabricate(:category)
-            put :update, params: { id: category.to_param,
-                                   category: invalid_attributes }
-            expect(response).to redirect_to(reposition_categories_path)
+          context "for json request" do
+            it "doesn't update the requested category" do
+              category = Fabricate(:category)
+              expect do
+                put :update, params: { id: category.to_param,
+                                       category: invalid_attributes },
+                             as: :json
+                category.reload
+              end.not_to change(category, :position)
+            end
+
+            it "responds with unprocessable_content" do
+              category = Fabricate(:category)
+              put :update, params: { id: category.to_param,
+                                     category: invalid_attributes },
+                           as: :json
+              expect(response).to have_http_status(:unprocessable_content)
+            end
           end
         end
       end
@@ -77,11 +121,23 @@ RSpec.describe RepositionCategoriesController, type: :controller do
       context "for a #{employee_type}" do
         before { sign_in(Fabricate("user_#{employee_type}")) }
 
-        it "redirects to unauthorized" do
-          category = Fabricate(:category)
-          put :update, params: { id: category.to_param,
-                                 category: valid_attributes }
-          expect_to_be_unauthorized(response)
+        context "for html request" do
+          it "redirects to unauthorized" do
+            category = Fabricate(:category)
+            put :update, params: { id: category.to_param,
+                                   category: valid_attributes }
+            expect_to_be_unauthorized(response)
+          end
+        end
+
+        context "for json request" do
+          it "redirects to unauthorized" do
+            category = Fabricate(:category)
+            put :update, params: { id: category.to_param,
+                                   category: valid_attributes },
+                         as: :json
+            expect_to_be_forbidden(response)
+          end
         end
       end
     end
