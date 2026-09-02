1246be777a5e889674d77b015585e9afc41f3e41
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2026-09-01 18:05:34 -0700

    Copy reposition methods from category
    To allow repositioning by int

--- app/models/task_type.rb
+++ app/models/task_type.rb
@@ -5,25 +5,45 @@ class TaskType < ApplicationRecord
   acts_as_list
   default_scope { order(position: :asc) }
 
+  attr_accessor :new_position
+
   has_many :search_results, dependent: nil
 
   validates :icon, presence: true, inclusion: { in: ICON_OPTIONS }
   validates :name, presence: true, length: { maximum: 100 },
                    uniqueness: { case_sensitive: false }
   validates :color, presence: true, inclusion: { in: COLOR_OPTIONS }
+  validate :new_position_numericality
 
-  def reposition(direction)
-    case direction
-    when 'up'
-      return move_higher
-    when 'down'
-      return move_lower
-    end
+  def reposition
+    return false if new_position.blank?
 
+    insert_at(new_position) || false
+  rescue ArgumentError => e
+    logger.debug "ArgumentError: #{e}"
     false
   end
 
   def any_tasks?
     Task.where(task_type: self).any?
   end
+
+  private
+
+    def new_position_numericality
+      return if new_position.blank?
+
+      unless new_position.instance_of?(Integer)
+        return errors.add(:new_position, 'must be an integer')
+      end
+
+      unless new_position.positive?
+        return errors.add(:new_position, 'must greater than 0')
+      end
+
+      max_position = TaskType.count
+      return unless max_position && new_position > max_position
+
+      errors.add(:new_position, "must less than #{max_position + 1}")
+    end
 end
