2ceb48704745f85b64ca92fc6e67754844009b5b
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2026-09-01 18:08:20 -0700

    Update sortables.js
    Allow sorting issue & task types
    
    Fixes Task#82

--- app/javascript/entrypoints/sortables.js
+++ app/javascript/entrypoints/sortables.js
@@ -1,13 +1,15 @@
 import Sortable from 'sortablejs/Sortable.min.js';
 import { patch } from "@rails/request.js";
 
-const sortableCategories = 'categories-and-projects';
-const sortableProjects = 'category-projects-list';
+const sortableProjects = 'sortable-projects';
 
 document.addEventListener('turbo:load', function() {
-  for (let elem of document.getElementsByClassName(sortableCategories)) {
-    new Sortable(elem, {
-      handle: '.category-name',
+  const categories = document.getElementById('sortable-categories');
+  const issueTypes = document.getElementById('sortable-issue-types');
+  const taskTypes = document.getElementById('sortable-task-types');
+
+  if (categories) {
+    new Sortable(categories, {
       onEnd: rePositionCategory
     })
   }
@@ -17,28 +19,47 @@ document.addEventListener('turbo:load', function() {
       onEnd: rePositionProject
     })
   }
+
+  if (issueTypes) {
+    new Sortable(issueTypes, {
+      onEnd: rePositionIssueType
+    })
+  }
+
+  if (taskTypes) {
+    new Sortable(taskTypes, {
+      onEnd: rePositionTaskType
+    })
+  }
 });
 
-function rePositionCategory(event) {
+function rePosition(event, paramKey) {
   const { oldIndex, newIndex, item } = event;
   const url = item.dataset["sortableUrl"];
 
   if (oldIndex == newIndex) return;
 
+  let params = {};
+  params[paramKey] = { new_position: newIndex + 1 };
+
   patch(url, {
-    body: JSON.stringify({ category: { new_position: newIndex + 1 } }),
+    body: JSON.stringify(params),
     responseKind: "json"
   });
 }
 
+function rePositionCategory(event) {
+  rePosition(event, 'category')
+}
+
 function rePositionProject(event) {
-  const { oldIndex, newIndex, item } = event;
-  const url = item.dataset["sortableUrl"];
+  rePosition(event, 'project')
+}
 
-  if (oldIndex == newIndex) return;
+function rePositionIssueType(event) {
+  rePosition(event, 'issue_type')
+}
 
-  patch(url, {
-    body: JSON.stringify({ project: { new_position: newIndex + 1 } }),
-    responseKind: "json"
-  });
+function rePositionTaskType(event) {
+  rePosition(event, 'task_type')
 }
--- spec/views/issue_types/index.html.erb_spec.rb
+++ spec/views/issue_types/index.html.erb_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe "issue_types/index", type: :view do
   let(:first_task_type) { Fabricate(:task_type) }
   let(:second_task_type) { Fabricate(:task_type) }
   let(:admin) { Fabricate(:user_admin) }
+  let(:reviewer) { Fabricate(:user_reviewer) }
 
   before do
     Fabricate(:issue, issue_type: first_issue_type)
@@ -19,32 +20,35 @@ RSpec.describe "issue_types/index", type: :view do
     before { enable_can(view, admin) }
 
     it "renders a list of issue_types" do
+      first_url = reposition_issue_type_url(first_issue_type.to_param)
+      second_url = reposition_issue_type_url(second_issue_type.to_param)
+
       render
-      assert_select "#issue-type-#{first_issue_type.id}", count: 1
-      assert_select "#issue-type-#{second_issue_type.id}", count: 1
-      assert_select "#task-type-#{first_task_type.id}", count: 1
-      assert_select "#task-type-#{second_task_type.id}", count: 1
+
+      assert_select(
+        "#issue-type-#{first_issue_type.id}[data-sortable-url='#{first_url}']",
+        count: 1
+      )
+      assert_select(
+        "#issue-type-#{second_issue_type.id}" \
+        "[data-sortable-url='#{second_url}']",
+        count: 1
+      )
     end
 
-    it "renders sorting links" do
+    it "renders a list of task_types" do
+      first_url = reposition_task_type_url(first_task_type.to_param)
+      second_url = reposition_task_type_url(second_task_type.to_param)
+
       render
 
-      expect(rendered).to have_link(
-        nil,
-        href: reposition_issue_type_path(first_issue_type, sort: "down")
+      assert_select(
+        "#task-type-#{first_task_type.id}[data-sortable-url='#{first_url}']",
+        count: 1
       )
-      expect(rendered).to have_link(
-        nil,
-        href: reposition_issue_type_path(second_issue_type, sort: "up")
-      )
-
-      expect(rendered).to have_link(
-        nil,
-        href: reposition_task_type_path(first_task_type, sort: "down")
-      )
-      expect(rendered).to have_link(
-        nil,
-        href: reposition_task_type_path(second_task_type, sort: "up")
+      assert_select(
+        "#task-type-#{second_task_type.id}[data-sortable-url='#{second_url}']",
+        count: 1
       )
     end
 
@@ -129,6 +133,130 @@ RSpec.describe "issue_types/index", type: :view do
         render
 
         expect(rendered).to have_link(
+          nil,
+          href: task_type_path(second_task_type)
+        )
+      end
+    end
+  end
+
+  context "for an reviewer" do
+    before { enable_can(view, reviewer) }
+
+    it "renders a list of issue_types" do
+      first_url = reposition_issue_type_url(first_issue_type.to_param)
+      second_url = reposition_issue_type_url(second_issue_type.to_param)
+
+      render
+
+      assert_select(
+        "#issue-type-#{first_issue_type.id}[data-sortable-url='#{first_url}']",
+        count: 1
+      )
+      assert_select(
+        "#issue-type-#{second_issue_type.id}" \
+        "[data-sortable-url='#{second_url}']",
+        count: 1
+      )
+    end
+
+    it "renders a list of task_types" do
+      first_url = reposition_task_type_url(first_task_type.to_param)
+      second_url = reposition_task_type_url(second_task_type.to_param)
+
+      render
+
+      assert_select(
+        "#task-type-#{first_task_type.id}[data-sortable-url='#{first_url}']",
+        count: 1
+      )
+      assert_select(
+        "#task-type-#{second_task_type.id}[data-sortable-url='#{second_url}']",
+        count: 1
+      )
+    end
+
+    it "renders new type links" do
+      render
+
+      expect(rendered).to have_link("New Issue Type", href: new_issue_type_path)
+      expect(rendered).to have_link("New Task Type", href: new_task_type_path)
+    end
+
+    context "when issue_type has an issue" do
+      it "renders migration link" do
+        render
+
+        expect(rendered).to have_link(
+          "Migrate issues",
+          href: new_issue_type_migration_path(first_issue_type)
+        )
+      end
+
+      it "doesn't render destroy link" do
+        render
+
+        expect(rendered).not_to have_link(
+          nil,
+          href: issue_type_path(first_issue_type)
+        )
+      end
+    end
+
+    context "when issue_type doesn't have an issue" do
+      it "doesn't render migration link" do
+        render
+
+        expect(rendered).not_to have_link(
+          nil,
+          href: new_issue_type_migration_path(second_issue_type)
+        )
+      end
+
+      it "doesn't render destroy link" do
+        render
+
+        expect(rendered).not_to have_link(
+          nil,
+          href: issue_type_path(second_issue_type)
+        )
+      end
+    end
+
+    context "when task_type has an task" do
+      it "renders migration link" do
+        render
+
+        expect(rendered).to have_link(
+          "Migrate tasks",
+          href: new_task_type_migration_path(first_task_type)
+        )
+      end
+
+      it "doesn't render destroy link" do
+        render
+
+        expect(rendered).not_to have_link(
+          nil,
+          href: task_type_path(first_task_type)
+        )
+      end
+    end
+
+    context "when task_type doesn't have an task" do
+      it "doesn't render migration link" do
+        render
+
+        expect(rendered).not_to have_link(
+          nil,
+          href: new_task_type_migration_path(second_task_type)
+        )
+      end
+
+      it "doesn't render destroy link" do
+        render
+
+        expect(rendered).not_to have_link(
           nil,
           href: task_type_path(second_task_type)
         )
