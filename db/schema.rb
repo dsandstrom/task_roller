fba9270c2c034a58af0581e36debeb3b7eca3eeb
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2026-09-10 10:07:58 -0700

    Add issue_branches table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_07_222926) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_10_170242) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -39,6 +39,15 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_07_222926) do
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
     t.index ["category_id", "user_id"], name: "index_category_tasks_subscriptions_on_category_id_and_user_id", unique: true
+  end
+
+  create_table "issue_branches", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.integer "source_issue_id"
+    t.integer "source_task_id"
+    t.integer "target_id", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
   end
 
   create_table "issue_closures", force: :cascade do |t|
